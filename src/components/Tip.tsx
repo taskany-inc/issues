3e7d91351ae057ed97c56871fe7d62d885b2a68a
Anton Vinogradov <winogradovaa@gmail.com>
2022-04-04 17:28:28 +0300

    this is Tip

---
+++ src/components/Tip.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+interface TipProps {
+    title?: string;
+    icon?: React.ReactNode;
+}
+
+const StyledTip = styled.div`
+    padding: 12px 0;
+    font-size: 13px;
+`;
+
+const StyledTipIcon = styled.span`
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 6px;
+`;
+
+const StyledTipTitle = styled.span`
+    font-weight: 600;
+    margin-right: 6px;
+`;
+
+const StyledTipText = styled.span`
+`;
+
+export const Tip: React.FC<TipProps> = ({ children, title, icon }) => {
+    return (
+        <StyledTip>
+            {icon && <StyledTipIcon>{icon}</StyledTipIcon>}
+            {title && <StyledTipTitle>{title}</StyledTipTitle>}
+            <StyledTipText>{children}</StyledTipText>
+        </StyledTip>
+    );
+}
