b6a84bab31701200ddc42e1b94da1d31fb803771
Anton Vinogradov <winogradovaa@gmail.com>
2022-04-21 22:19:53 +0300

    hex generator with background contrast checking

---
+++ src/utils/randomHex.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ColorContrastChecker = require('color-contrast-checker');
+
+const ccc = new ColorContrastChecker();
+
+const generateHex = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+export const randomHex = (bg: [string, string], ratio = 6) => {
+    let generatedColor: string | null = null;
+
+    const colorsMap = bg.reduce((acc, color) => {
+        acc[color] = false;
+        return acc;
+    }, {} as Record<string, string | boolean>);
+
+    while (!colorsMap[0] && !colorsMap[1]) {
+        generatedColor = generateHex();
+
+        try {
+            colorsMap[0] = ccc.isLevelCustom(generatedColor, bg[0], ratio);
+            colorsMap[1] = ccc.isLevelCustom(generatedColor, bg[1], ratio);
+        } catch (e) {
+            colorsMap[0] = ccc.isLevelCustom(generatedColor, bg[0], ratio);
+            colorsMap[1] = ccc.isLevelCustom(generatedColor, bg[1], ratio);
+        }
+    }
+
+    return generatedColor;
+};
