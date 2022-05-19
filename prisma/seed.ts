c1617392bb92eb3cc90a521dd8784d36fa69b80b
Anton Vinogradov <winogradovaa@gmail.com>
2022-05-20 01:04:30 +0300

    fix seed script for env variables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,23 +2,29 @@ import assert from 'assert';
 
 import { prisma } from '../src/utils/prisma';
 
+const adminEmail = process.env.ADMIN_EMAIL || 'tony@taskany.org';
+const adminPassword = process.env.ADMIN_PASSWORD || 'taskany';
+
+async function seed(title: string, cb: () => void) {
+    console.log('SEED:', title, '...');
+    await cb();
+    console.log('done!');
+}
+
 seed('Admin user', async () => {
-    assert(process.env.ADMIN_EMAIL, "Admin's email isn't provided. Check your environment variables: ADMIN_EMAIL.");
-    assert(
-        process.env.ADMIN_PASSWORD,
-        "Admin's password isn't provided. Check your environment variables: ADMIN_PASSWORD.",
-    );
+    assert(adminEmail, "Admin's email isn't provided. Check your environment variables: ADMIN_EMAIL.");
+    assert(adminPassword, "Admin's password isn't provided. Check your environment variables: ADMIN_PASSWORD.");
 
     await prisma.user.create({
         data: {
-            email: process.env.ADMIN_EMAIL!,
+            email: adminEmail,
             role: 'ADMIN',
             accounts: {
                 create: {
                     type: 'credentials',
                     provider: 'credentials',
                     providerAccountId: 'credentials',
-                    password: process.env.ADMIN_PASSWORD!,
+                    password: adminPassword,
                 },
             },
         },
@@ -39,9 +45,3 @@ seed('Default flows and states', async () => {
         },
     });
 });
-
-async function seed(title: string, cb: () => void) {
-    console.log('SEED:', title, '...');
-    await cb();
-    console.log('done!');
-}
