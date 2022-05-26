59845c638856dddc6abfc717dcef7c1ffd18d063
Tony Vi <winogradovaa@gmail.com>
2022-05-26 22:37:43 +0300

    this is the PageSep component

---
+++ src/components/PageSep.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { gray4 } from '../design/@generated/themes';
+
+export const PageSep = styled.div`
+    border-top: 1px solid ${gray4};
+    margin: 16px -40px; /* Page component padding */
+`;
