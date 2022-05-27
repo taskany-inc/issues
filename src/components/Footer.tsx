9560a1f5e6929a09c701cb8cd4ff3130d32faa17
Katrinputrina <putrina.katya@gmail.ru>
2022-05-27 22:25:20 +0300

    correction of comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
+
 import { SheepLogo } from './SheepLogo';
 
 const StyledFooter = styled.footer`
     padding: 20px 20px;
     display: flex;
     justify-content: start;
-    max-height: 60px;
+    height: 60px;
 `;
 
 const StyledFooterText = styled.span`
-    font-weight: 400;
     color: rgba(255, 255, 255, 0.6);
 `;
 
-export const Footer: React.FC = () => {
+export const Footer = () => {
     return (
         <>
             <StyledFooter>
                 <StyledFooterText>
                     {`© ${new Date().getFullYear()} Taskany, Inc.`}
-                        <SheepLogo />
+                    <SheepLogo />
                 </StyledFooterText>
             </StyledFooter>
         </>
