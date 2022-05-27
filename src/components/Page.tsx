fabc01e64be4a1f3209da3dd96037c821270fd0c
Katrinputrina <putrina.katya@gmail.ru>
2022-05-27 22:25:20 +0300

    correction of comments

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
 
 import { pageContext } from '../utils/pageContext';
 import { ExternalPageProps } from '../utils/ssrProps';
 
 import { Header } from './Header';
 import { Footer } from './Footer';
-import styled from 'styled-components';
 
 interface PageProps {
     locale: ExternalPageProps['locale'];
@@ -14,6 +14,7 @@ interface PageProps {
 }
 
 const StyledContent = styled.div`
+    /* presses the footer to the bottom*/
     min-height: calc(100vh - 160px);
 `;
 
