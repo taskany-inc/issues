ac157a0ad285c9b29901d064c8f97827ac108f33
Tony Vi <winogradovaa@gmail.com>
2022-06-06 22:15:14 +0300

    rewrite Button with generated colors

--- src/components/UserPic.tsx
+++ src/components/UserPic.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { signIn } from 'next-auth/react';
 
 import { Icon } from './Icon';
 
 interface UserPicProps {
     src?: string | null;
     size?: number;
+    onClick?: () => void;
 }
 
 const StyledImage = styled.img`
     border: 0;
     border-radius: 100%;
-    justify-self: center;
 `;
 
-export const UserPic: React.FC<UserPicProps> = ({ src, size = 32 }) => {
+export const UserPic: React.FC<UserPicProps> = ({ src, size = 32, onClick }) => {
     const sizePx = `${size}px`;
 
     return src ? (
         <StyledImage src={src} height={sizePx} width={sizePx} />
     ) : (
-        <Icon type="user" size="s" onClick={() => signIn()} />
+        <Icon type="user" size={size} onClick={onClick} />
     );
 };
