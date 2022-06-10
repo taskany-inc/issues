1518aa93949d9e024dd768bebe709c9d5769bbf9
Tony Vi <winogradovaa@gmail.com>
2022-06-10 23:20:59 +0300

    use Modal instead of @geist-ui

--- src/components/GoalCreateModal.tsx
+++ src/components/GoalCreateModal.tsx
@@ -5,7 +5,7 @@ import tinykeys from 'tinykeys';
 import { routes, useRouter } from '../hooks/router';
 import { createHotkeys, createGoalKeys } from '../utils/hotkeys';
 
-import { DialogModal } from './DialogModal';
+import { Modal } from './Modal';
 import { GoalCreateForm } from './GoalCreateForm';
 
 export const GoalCreateModal = () => {
@@ -19,8 +19,8 @@ export const GoalCreateModal = () => {
     useEffect(() => tinykeys(window, createHotkeys([createGoalKeys, () => showModalOrNavigate(router.createGoal)])));
 
     return (
-        <DialogModal visible={modalVisible} onClose={onModalClose}>
+        <Modal visible={modalVisible} onClose={onModalClose}>
             <GoalCreateForm onCreate={(id) => id && router.goal(id)} />
-        </DialogModal>
+        </Modal>
     );
 };
