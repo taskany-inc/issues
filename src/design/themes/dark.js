b156e754340420fbe1c56f39d5f614dc939c4703
Tony Vi <winogradovaa@gmail.com>
2022-06-11 01:29:55 +0300

    remove part of hardcoded colors

--- src/design/themes/dark.js
+++ src/design/themes/dark.js
@@ -5,10 +5,5 @@ module.exports = {
     ...dark,
 
     // old non generated colors
-    formInputBackgroundColor: 'rgba(0, 0, 0, .35)',
-    formInputErrorColor: '#e00',
-    popupDangerBackgroundColor: '#e00',
-    popupWarningBackgroundColor: '#f5a623',
-
     linkTextColor: '#00C2FF',
 };
