2c741a22f9b15835e4b13152b89cf476379c1d1e
Tony Vi <winogradovaa@gmail.com>
2022-06-21 12:28:11 +0300

    optimize theme generation

--- src/design/themes/dark.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { dark } = require('../palette');
-
-module.exports = {
-    ...dark,
-};
--- src/design/themes/light.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { light } = require('../palette');
-
-module.exports = {
-    ...light,
-};
