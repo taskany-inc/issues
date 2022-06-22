4aa4732aab1b75f50dcbedf3910215646d64fc3e
Tony Vi <winogradovaa@gmail.com>
2022-06-22 15:14:06 +0300

    block hotkeys in inputs and textarea controls

--- src/utils/hotkeys.ts
+++ src/utils/hotkeys.ts
@@ -5,14 +5,26 @@ export const showProjectsKeys = ['g p', 'п з'];
 export const showGoalsKeys = ['g g', 'п п'];
 export const inviteUserKeys = ['c u', 'с г'];
 
-type HotkeyDeclaration = [string[], () => void];
-export const createHotkeys = (...args: HotkeyDeclaration[]): Record<string, () => void> => {
-    const declarations: Record<string, () => void> = {};
+function isEventTargetInputOrTextArea(eventTarget: EventTarget | null) {
+    if (eventTarget === null) return false;
+
+    const eventTargetTagName = (eventTarget as HTMLElement).tagName.toLowerCase();
+    return ['input', 'textarea'].includes(eventTargetTagName);
+}
+
+type HotkeyDeclaration = [string[], (e: KeyboardEvent) => void];
+export const createHotkeys = (...args: HotkeyDeclaration[]): Record<string, (e: KeyboardEvent) => void> => {
+    const declarations: Record<string, (e: KeyboardEvent) => void> = {};
 
     args.forEach((decl) => {
         decl[0].forEach((key) => {
+            // https://github.com/jamiebuilds/tinykeys/issues/17#issuecomment-847867271
             // eslint-disable-next-line prefer-destructuring
-            declarations[key] = decl[1];
+            declarations[key] = (e: KeyboardEvent) => {
+                if (!isEventTargetInputOrTextArea(e.target)) {
+                    decl[1](e);
+                }
+            };
         });
     });
 
