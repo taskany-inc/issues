7e5affe5431ea8c6125e1f6eec9c3edddeb945cd
Tony Vi <winogradovaa@gmail.com>
2022-06-22 22:04:37 +0300

    fix PageSep extra margin

--- src/components/PageSep.tsx
+++ src/components/PageSep.tsx
@@ -4,5 +4,5 @@ import { gray4 } from '../design/@generated/themes';
 
 export const PageSep = styled.div`
     border-top: 1px solid ${gray4};
-    margin: 16px -40px; /* Page component padding */
+    margin: 16px 0px;
 `;
