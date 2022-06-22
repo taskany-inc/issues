2db4adbee87688b27b056bf803166c4886c56fb6
Tony Vi <winogradovaa@gmail.com>
2022-06-22 18:35:42 +0300

    Keyboard component design

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { gray4, gray8, radiusS, textColor } from '../design/@generated/themes';
+import { gray4, gray8, gray9, radiusM } from '../design/@generated/themes';
 
 interface KeyboardProps {
     command?: boolean;
@@ -12,15 +12,30 @@ interface KeyboardProps {
 }
 
 const StyledKeyboard = styled.kbd`
+    display: inline-block;
+    padding: 2px 5px;
+
+    font-family: ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace;
     line-height: 1.1em;
     text-align: center;
-    display: inline-block;
-    color: ${textColor};
+    color: ${gray9};
+
+    cursor: pointer;
+
     background-color: ${gray4};
-    font-family: sans-serif;
-    border-radius: ${radiusS};
+
+    border-radius: ${radiusM};
+
     border: 1px solid ${gray8};
-    padding: 4px 6px;
+
+    box-shadow: inset 0 -1px 0 ${gray8};
+
+    transition: 200ms cubic-bezier(0.3, 0, 0.5, 1);
+    transition-property: box-shadow;
+
+    &:hover {
+        box-shadow: inset 0 0px 0 ${gray8};
+    }
 
     span {
         font-size: inherit;
