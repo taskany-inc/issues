7df6a81b70e379a88109052211fe304c3b785218
Tony Vi <winogradovaa@gmail.com>
2022-06-23 14:59:21 +0300

    remove create pages

--- src/hooks/router.ts
+++ src/hooks/router.ts
@@ -3,14 +3,9 @@ import { useRouter as NextRouter } from 'next/router';
 export const routes = {
     index: () => '/',
     goals: () => '/goals',
-    createGoal: () => '/goals/new',
     goal: (id: string) => `/goals/${id}`,
-    editGoal: (id: string) => `/goals/${id}/edit`,
-    createProject: () => '/projects/new',
     projects: () => '/projects',
     project: (id: string) => `/projects/${id}`,
-    editProject: (id: string) => `/projects/${id}/edit`,
-    inviteUsers: () => '/users/invite',
     userSettings: () => '/users/settings',
     signIn: () => '/api/auth/signin',
 };
@@ -22,13 +17,8 @@ export const useRouter = () => {
         index: () => router.push(routes.index()),
         projects: () => router.push(routes.projects()),
         project: (id: string) => router.push(routes.project(id)),
-        createProject: () => router.push(routes.createProject()),
-        editProject: (id: string) => router.push(routes.editProject(id)),
         goals: () => router.push(routes.goals()),
         goal: (id: string) => router.push(routes.goal(id)),
-        createGoal: () => router.push(routes.createGoal()),
-        editGoal: (id: string) => router.push(routes.editGoal(id)),
-        inviteUsers: () => router.push(routes.inviteUsers()),
         userSettings: () => router.push(routes.userSettings()),
     };
 };
