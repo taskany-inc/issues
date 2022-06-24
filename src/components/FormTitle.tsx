852d85dda343396e7661c33061caae386ec710a9
Tony Vi <winogradovaa@gmail.com>
2022-06-24 15:29:51 +0300

    this is FormTitle

---
+++ src/components/FormTitle.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+import { gapM, gapS } from '../design/@generated/themes';
+
+import { Text } from './Text';
+
+export const FormTitle = styled(Text)`
+    padding: ${gapS} 0 ${gapM};
+`;
+
+FormTitle.defaultProps = {
+    size: 'xl',
+    weight: 'bolder',
+};
