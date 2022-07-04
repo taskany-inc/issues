2e9873ebed60ceaa96f9be0fb5f8d1b75cc62d68
Tony Vi <winogradovaa@gmail.com>
2022-07-05 00:25:36 +0300

    issuer and owner are participants by default

--- graphql/types.ts
+++ graphql/types.ts
@@ -233,7 +233,7 @@ export const GoalCreateInput = inputObjectType({
         t.field(GoalModel.projectId);
         t.field(GoalModel.stateId);
         t.list.field('tags', { type: TagInput });
-        // t.list.field('participants', { type: Activity });
+        t.list.field('participants', { type: ActivityInput });
         // t.list.field('dependsOn', { type: Goal });
         // t.list.field('blocks', { type: Goal });
         // t.list.field('relatedTo', { type: Goal });
@@ -255,7 +255,7 @@ export const GoalInput = inputObjectType({
         t.field(GoalModel.projectId);
         t.field(GoalModel.stateId);
         t.list.field('tags', { type: TagInput });
-        // t.list.field('participants', { type: Activity });
+        t.list.field('participants', { type: ActivityInput });
         // t.list.field('dependsOn', { type: Goal });
         // t.list.field('blocks', { type: Goal });
         // t.list.field('relatedTo', { type: Goal });
