46b0888c540c8ab79424afa0fe766ac087d13ab0
Tony Vi <winogradovaa@gmail.com>
2022-08-18 00:28:02 +0300

    multiple invites form view

--- src/components/UserInviteForm.tsx
+++ src/components/UserInviteForm.tsx
@@ -1,11 +1,13 @@
+import { useCallback, useState, useRef } from 'react';
 import { useTranslations } from 'next-intl';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
 import toast from 'react-hot-toast';
 import z from 'zod';
+import { FieldError } from 'react-hook-form';
+import styled from 'styled-components';
 
+import { gapM, gray7, star0 } from '../design/@generated/themes';
 import { gql } from '../utils/gql';
-import { star0 } from '../design/@generated/themes';
+import { KeyCode } from '../hooks/useKeyboard';
 
 import { Icon } from './Icon';
 import { Button } from './Button';
@@ -15,43 +17,32 @@ import { Form } from './Form';
 import { Tip } from './Tip';
 import { Keyboard } from './Keyboard';
 import { FormTitle } from './FormTitle';
+import { Tag } from './Tag';
+import { Text } from './Text';
 
 interface UserInviteFormProps {
     onCreate?: (id?: string) => void;
 }
 
+const StyledEmails = styled.div`
+    padding: ${gapM} 0;
+`;
+
 export const UserInviteForm: React.FC<UserInviteFormProps> = ({ onCreate }) => {
     const t = useTranslations('users.invite');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [emails, setEmails] = useState<string[]>([]);
+    const [error, setError] = useState<FieldError>();
 
-    const schema = z.object({
-        email: z
-            .string({
-                required_error: t('User email is required'),
-                invalid_type_error: t('User email must be a string'),
-            })
-            .email({
-                message: t('User email is required'),
-            }),
+    const emailSchema = z.string().email({
+        message: t('User email is required'),
     });
 
-    type FormType = z.infer<typeof schema>;
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isSubmitted },
-    } = useForm<FormType>({
-        resolver: zodResolver(schema),
-        mode: 'onChange',
-        reValidateMode: 'onChange',
-        shouldFocusError: true,
-    });
-
-    const inviteUser = async ({ email }: FormType) => {
+    const inviteUser = async () => {
         const promise = gql.mutation({
             inviteUser: [
                 {
-                    email,
+                    emails,
                 },
                 {
                     id: true,
@@ -63,7 +54,7 @@ export const UserInviteForm: React.FC<UserInviteFormProps> = ({ onCreate }) => {
         toast.promise(promise, {
             error: t('Something went wrong 😿'),
             loading: t('We are creating invite...'),
-            success: t('Voila! User with email {email} invited 🎉', { email }),
+            success: t('Voila! Users invited 🎉'),
         });
 
         const res = await promise;
@@ -71,29 +62,83 @@ export const UserInviteForm: React.FC<UserInviteFormProps> = ({ onCreate }) => {
         onCreate && onCreate(res.inviteUser?.id);
     };
 
+    const onInputChange = useCallback(() => {
+        setError(undefined);
+    }, []);
+
+    const onEmailRemove = useCallback(
+        (email: string) => () => {
+            setEmails(emails.filter((em) => em !== email));
+        },
+        [setEmails, emails],
+    );
+
+    const onInputKeyDown = useCallback(
+        (e: React.KeyboardEvent) => {
+            if ((e.keyCode === KeyCode.Enter || e.keyCode === KeyCode.Space) && inputRef.current) {
+                e.preventDefault();
+                const possibleEmail = inputRef.current.value;
+
+                try {
+                    emailSchema.parse(possibleEmail);
+                    setEmails([...emails, possibleEmail]);
+                    inputRef.current.value = '';
+                } catch (err: unknown) {
+                    if (err instanceof Error) {
+                        // Zod serialize errors to message field ¯\_(ツ)_/¯
+                        const errors = JSON.parse(err.message);
+                        setError(errors[0]);
+                    }
+                }
+            }
+        },
+        [emailSchema, setEmails, emails],
+    );
+
+    const isValid = emails.length > 0;
+
     return (
         <>
-            <FormTitle>{t('Invite new user')}</FormTitle>
+            <FormTitle>{t('Invite new users')}</FormTitle>
 
-            <Form onSubmit={handleSubmit(inviteUser)}>
+            <StyledEmails>
+                {isValid ? (
+                    <>
+                        {emails.map((email) => (
+                            <Tag key={email} title={email} onHide={onEmailRemove(email)} />
+                        ))}
+                    </>
+                ) : (
+                    <Text size="s" color={gray7}>
+                        {t.rich('Start typing users emails', {
+                            key1: () => <Keyboard enter />,
+                            key2: () => <Keyboard space />,
+                        })}
+                    </Text>
+                )}
+            </StyledEmails>
+
+            <Form onSubmit={() => inviteUser}>
                 <FormInput
-                    {...register('email')}
-                    error={isSubmitted ? errors.email : undefined}
-                    placeholder={t('User email')}
+                    ref={inputRef}
+                    error={error}
+                    placeholder={t('Users emails')}
                     autoFocus
                     flat="bottom"
+                    onChange={onInputChange}
+                    onKeyDown={onInputKeyDown}
                 />
 
                 <FormActions flat="top">
                     <FormAction left />
                     <FormAction right inline>
-                        <Button size="m" view="primary" type="submit" disabled={!isValid} text={t('Send invite')} />
+                        <Button size="m" view="primary" type="submit" disabled={!isValid} text={t('Send invites')} />
                     </FormAction>
                 </FormActions>
             </Form>
 
             <Tip title={t('Pro tip!')} icon={<Icon type="bulbOn" size="s" color={star0} />}>
-                {t.rich('Press key to send invite', {
+                {t.rich('Press key to send invites', {
                     key: () => <Keyboard command enter />,
                 })}
             </Tip>
