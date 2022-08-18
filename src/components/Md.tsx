58b2019c020e892acd5dc1d246bd5e6445b365e1
Tony Vi <winogradovaa@gmail.com>
2022-08-19 00:57:10 +0300

    fix injecting css warn

--- src/components/Md.tsx
+++ src/components/Md.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Head from 'next/head';
 
 import { link10 } from '../design/@generated/themes';
 import { md } from '../utils/md';
@@ -26,16 +25,4 @@ interface MdProps {
     children?: string;
 }
 
-export const Md: React.FC<MdProps> = ({ children }) => (
-    <>
-        <Head>
-            <link
-                rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css"
-                crossOrigin="anonymous"
-            />
-        </Head>
-
-        <StyledMd dangerouslySetInnerHTML={{ __html: md(children) }} />
-    </>
-);
+export const Md: React.FC<MdProps> = ({ children }) => <StyledMd dangerouslySetInnerHTML={{ __html: md(children) }} />;
