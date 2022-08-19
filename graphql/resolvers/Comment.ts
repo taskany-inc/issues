6b9944d37a9a2d785d88580f38cc0a8d133728c7
Tony Vi <winogradovaa@gmail.com>
2022-08-19 15:08:12 +0300

    add mail sapport for MailServer, debug in comment resolver

--- graphql/resolvers/Comment.ts
+++ graphql/resolvers/Comment.ts
@@ -2,6 +2,7 @@ import { nonNull, stringArg } from 'nexus';
 import { ObjectDefinitionBlock } from 'nexus/dist/core';
 
 import { Comment, computeUserFields, withComputedField } from '../types';
+import { mailServer } from '../../src/utils/mailServer';
 
 export const query = (t: ObjectDefinitionBlock<'Query'>) => {
     t.list.field('goalComment', {
@@ -38,19 +39,27 @@ export const mutation = (t: ObjectDefinitionBlock<'Mutation'>) => {
         resolve: async (_, { goalId, description, authorId }, { db }) => {
             const [commentAuthor, goal] = await Promise.all([
                 db.user.findUnique({ where: { id: authorId }, include: { activity: true } }),
-                db.goal.findUnique({ where: { id: goalId } }),
+                db.goal.findUnique({ where: { id: goalId }, include: { participants: { ...computeUserFields } } }),
             ]);
 
             if (!commentAuthor || !commentAuthor.activity) return null;
             if (!goal) return null;
 
             try {
-                const newComment = db.comment.create({
+                const newComment = await db.comment.create({
                     data: {
                         description,
                         activityId: commentAuthor.activity.id,
                         goalId,
                     },
+                });
+
+                await mailServer.sendMail({
+                    from: '"Taskany Issues" <notify@taskany.org>',
+                    to: goal.participants.map((p) => p.user?.email).join(' ,'),
+                    subject: 'Hello ✔',
+                    text: `new comment for ${process.env.NEXTAUTH_URL}/goals/${goalId}#comment-${newComment.id}`,
+                    html: `<a href="${process.env.NEXTAUTH_URL}/goals/${goalId}#comment-${newComment.id}">new comment</a> for <a href="${process.env.NEXTAUTH_URL}/goals/${goalId}">${goalId}</a>`,
                 });
 
                 return newComment;
