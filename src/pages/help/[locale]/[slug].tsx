794fb8572a9cf1a1f5dea60d951e26d5077b5f03
Tony Vi <winogradovaa@gmail.com>
2022-08-24 00:49:27 +0300

    md based help center

---
+++ src/pages/help/[locale]/[slug].tsx
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import NextLink from 'next/link';
+
+import { ExternalPageProps } from '../../../utils/declareSsrProps';
+import { Page } from '../../../components/Page';
+import { Md } from '../../../components/Md';
+
+type TLocale = 'en' | 'ru';
+const safeUrl = (url: string) => url.replace('/', '__');
+const sourcesDir = (locale: TLocale) => `${process.cwd()}/src/pages/help/source/${locale}`;
+const sourcesExt = '.md';
+const getLink = (locale: TLocale, slug: string) => `/help/${locale}/${slug}`;
+
+interface YmlProps {
+    title: string;
+}
+
+interface HelpMenuProps {
+    slug: string;
+    link: string;
+}
+
+export async function getStaticPaths({ locales }: { locales: TLocale[] }) {
+    const sourcesNames: Array<Record<string, string>> = [];
+
+    for (const locale of locales) {
+        sourcesNames.push(...fs.readdirSync(sourcesDir(locale)).map((fileName) => ({ locale, fileName })));
+    }
+
+    return {
+        paths: sourcesNames.map(({ locale, fileName }) => ({
+            params: { locale, slug: safeUrl(fileName.replace(sourcesExt, '')) },
+        })),
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params: { locale, slug } }: { params: { locale: TLocale; slug: string } }) {
+    const filePath = `${sourcesDir(locale || 'en')}/${slug}${sourcesExt}`;
+    const orig = fs.readFileSync(filePath, 'utf-8');
+    const stat = JSON.parse(JSON.stringify(fs.statSync(filePath)));
+    const { data: yml, content: source } = matter(orig);
+    const menu = fs.readdirSync(sourcesDir(locale)).map((fileName) => {
+        const linkSlug = fileName.replace(sourcesExt, '');
+        return { slug: linkSlug, link: getLink(locale, linkSlug) };
+    });
+
+    return {
+        props: {
+            source,
+            stat,
+            yml,
+            menu,
+            i18n: (await import(`../../../../i18n/${locale}.json`)).default,
+        },
+        revalidate: 10,
+    };
+}
+
+const HomePage = ({
+    locale,
+    source,
+    stat,
+    yml,
+    menu,
+}: ExternalPageProps<{ source: string; stat: fs.Stats; yml: YmlProps }>) => {
+    return (
+        <Page locale={locale} title={yml.title}>
+            Created: {stat.ctime}
+            <br />
+            Modified: {stat.mtime}
+            <br />
+            <Md>{source}</Md>
+            <div>
+                {menu.map(({ slug, link }: HelpMenuProps) => (
+                    <NextLink key={slug} href={link}>
+                        {slug}
+                    </NextLink>
+                ))}
+            </div>
+        </Page>
+    );
+};
+
+export default HomePage;
