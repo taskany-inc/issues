6206b20db417c25bfa6f3a115121f3b4e302cdd3
Farsiev Bogdan <fbogdan13@gmail.com>
2022-10-06 11:21:29 +0300

    card overlapping by action pannel fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,7 +16,7 @@ export const Card = styled.div`
 export const CardContent = styled.div`
     position: relative;
     box-sizing: border-box;
-    padding: 12px 14px;
+    padding: 12px 14px 60px;
 `;
 
 const StyledCardInfo = styled(Text)`
