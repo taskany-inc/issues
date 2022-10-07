5fba04b777f194a8c843c0a0367081527e628e95
Farsiev Bogdan <fbogdan13@gmail.com>
2022-10-07 15:25:31 +0300

    turning on prismjs for Md element

--- src/components/Md.tsx
+++ src/components/Md.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import { link10 } from '../design/@generated/themes';
 import { md } from '../utils/md';
 
+import { PrismCss } from './PrismStyle';
+
 const StyledMd = styled.div`
     a {
         color: inherit;
@@ -25,6 +27,11 @@ interface MdProps {
     children?: string;
 }
 
-const Md: React.FC<MdProps> = ({ children }) => <StyledMd dangerouslySetInnerHTML={{ __html: md(children) }} />;
+const Md: React.FC<MdProps> = ({ children }) => (
+    <>
+        <PrismCss />
+        <StyledMd dangerouslySetInnerHTML={{ __html: md(children) }} />
+    </>
+);
 
 export default Md;
