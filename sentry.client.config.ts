c8a9bbbbf37b244fd211f1ab3a4a5f08417b5f1d
Tony Vi <winogradovaa@gmail.com>
2022-10-31 23:27:48 +0300

    Sentry basic set up

---
+++ sentry.client.config.ts
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+if (SENTRY_DSN) {
+    /**
+     * Release must be passed with SENTRY_RELEASE variable.
+     */
+    Sentry.init({
+        dsn: SENTRY_DSN,
+        tracesSampleRate: 1.0,
+    });
+}
