1a8e563c06b4e6699a5ad939d4fa4323d07950dd
Tony Vi <winogradovaa@gmail.com>
2022-11-23 21:47:26 +0200

    rewrite LimitFilter w/ Dropdown

---
+++ src/components/LimitFilterDropdown.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+import { FiltersMenuItem } from './FiltersMenuItem';
+import { MenuItem } from './MenuItem';
+
+const Dropdown = dynamic(() => import('./Dropdown'));
+
+export const defaultLimit = 10;
+const limitVariants = [defaultLimit, 20, 30, 50, 100];
+
+interface LimitFilterDropdownProps {
+    text: React.ComponentProps<typeof Dropdown>['text'];
+    value?: number;
+    disabled?: React.ComponentProps<typeof Dropdown>['disabled'];
+
+    onChange?: (l: number) => void;
+}
+
+export const LimitFilterDropdown = React.forwardRef<HTMLDivElement, LimitFilterDropdownProps>(
+    ({ text, value = defaultLimit, disabled, onChange }, ref) => (
+        <Dropdown
+            ref={ref}
+            text={text}
+            value={value}
+            onChange={onChange}
+            items={limitVariants}
+            disabled={disabled}
+            renderTrigger={(props) => (
+                <FiltersMenuItem
+                    ref={props.ref}
+                    active={value !== defaultLimit}
+                    disabled={props.disabled}
+                    onClick={props.onClick}
+                >
+                    {props.text}
+                </FiltersMenuItem>
+            )}
+            renderItem={(props) => (
+                <MenuItem
+                    key={props.item}
+                    focused={props.cursor === props.index}
+                    selected={props.item === value}
+                    onClick={props.onClick}
+                >
+                    {props.item}
+                </MenuItem>
+            )}
+        />
+    ),
+);
--- src/hooks/useUrlParams.ts
+++ src/hooks/useUrlParams.ts
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-import { defaultLimit } from '../components/LimitFilter';
+import { defaultLimit } from '../components/LimitFilterDropdown';
 
 import { useMounted } from './useMounted';
 
