4ddc63518917f44d16735862ccf4a76a56dcfc21
Tony Vi <winogradovaa@gmail.com>
2022-11-24 22:58:49 +0200

    rewrite UserCompletionInput w/ ComboBox

--- src/components/IssueParticipantsForm.tsx
+++ src/components/IssueParticipantsForm.tsx
@@ -1,13 +1,20 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState, ChangeEvent } from 'react';
 import { useTranslations } from 'next-intl';
 import styled from 'styled-components';
+import { useSession } from 'next-auth/react';
+import useSWR from 'swr';
+import dynamic from 'next/dynamic';
 
 import { gapL, gapM } from '../design/@generated/themes';
 import { Activity, Goal } from '../../graphql/@generated/genql';
+import { createFetcher } from '../utils/createFetcher';
 
 import { FormTitle } from './FormTitle';
-import { UserCompletionInput } from './UserCompletionInput';
 import { IssueParticipantsList } from './IssueParticipantsList';
+import { FormInput } from './FormInput';
+import { UserMenuItem } from './UserMenuItem';
+
+const ComboBox = dynamic(() => import('./ComboBox'));
 
 interface IssueParticipantsFormProps {
     issue: Goal;
@@ -19,29 +26,61 @@ const StyledCompletion = styled.div`
     padding: ${gapL} 0 ${gapM};
 `;
 
+const fetcher = createFetcher((_, query: string, filter?: string[]) => ({
+    findActivity: [
+        {
+            data: {
+                query,
+                filter,
+            },
+        },
+        {
+            id: true,
+            user: {
+                id: true,
+                name: true,
+                email: true,
+                image: true,
+            },
+            ghost: {
+                id: true,
+                email: true,
+            },
+        },
+    ],
+}));
+
 export const IssueParticipantsForm: React.FC<IssueParticipantsFormProps> = ({ issue, onChange }) => {
+    const { data: session } = useSession();
     const t = useTranslations('IssueParticipants');
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+
+    const [query, setQuery] = useState('');
+    const [completionVisible, setCompletionVisible] = useState(false);
     const activities = useMemo(() => new Set<string>(issue.participants?.map((p) => p!.id)), [issue]);
+
+    const { data } = useSWR(query, (q) => fetcher(session?.user, q));
 
     const onParticipantDelete = useCallback(
         (id: string) => {
             activities.delete(id);
-            onChange && onChange(Array.from(activities));
+
+            onChange?.(Array.from(activities));
         },
         [activities, onChange],
     );
 
     const onParticipantAdd = useCallback(
         (activity: Activity) => {
-            if (activity) {
-                activities.add(activity.id);
-            }
+            activities.add(activity.id);
 
-            onChange && onChange(Array.from(activities));
+            setQuery('');
+
+            onChange?.(Array.from(activities));
         },
         [activities, onChange],
     );
+
+    const alreadyParticipants = issue.participants?.map((p) => p!.id);
 
     return (
         <>
@@ -54,10 +93,33 @@ export const IssueParticipantsForm: React.FC<IssueParticipantsFormProps> = ({ is
             />
 
             <StyledCompletion>
-                <UserCompletionInput
-                    placeholder={t('Add participants')}
-                    onClick={onParticipantAdd}
-                    filter={issue.participants?.map((p) => p!.id)}
+                <ComboBox
+                    text={query}
+                    value={query}
+                    visible={completionVisible}
+                    items={data?.findActivity?.filter((a) => !alreadyParticipants?.includes(a.id))}
+                    onChange={onParticipantAdd}
+                    renderInput={(props) => (
+                        <FormInput
+                            autoFocus
+                            placeholder={t('Add participants')}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                setQuery(e.currentTarget.value);
+                                setCompletionVisible(true);
+                            }}
+                            {...props}
+                        />
+                    )}
+                    renderItem={(props) => (
+                        <UserMenuItem
+                            key={props.item.id}
+                            name={props.item.user?.name}
+                            email={props.item.user?.email || props.item.ghost?.email}
+                            image={props.item.user?.image}
+                            focused={props.cursor === props.index}
+                            onClick={props.onClick}
+                        />
+                    )}
                 />
             </StyledCompletion>
         </>
