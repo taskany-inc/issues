be394caca6402353ffc000802042669ccf29e283
Tony Vi <winogradovaa@gmail.com>
2022-11-25 15:22:57 +0200

    rewrite GoalForm controls for validation

--- src/components/GoalCreateForm.tsx
+++ src/components/GoalCreateForm.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
 import toast from 'react-hot-toast';
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 
 import { gql } from '../utils/gql';
 import { gapS, gray6, star0 } from '../design/@generated/themes';
-import { Project, EstimateInput, State, Tag as TagModel, Activity, Priority } from '../../graphql/@generated/genql';
+import { Activity } from '../../graphql/@generated/genql';
 import { routes } from '../hooks/router';
 import { TLocale } from '../types/locale';
 
@@ -34,64 +34,21 @@ const StyledFormBottom = styled.div`
 const GoalCreateForm: React.FC<GoalCreateFormProps> = ({ locale, onCreate }) => {
     const t = useTranslations('goals.new');
     const { data: session } = useSession();
-    const [owner, setOwner] = useState({ id: session?.user.activityId, user: session?.user } as Activity);
-    const [estimate, setEstimate] = useState<EstimateInput>();
-    const [project, setProject] = useState<Project>();
-    const [state, setState] = useState<State>();
-    const [priority, setPriority] = useState<Priority>();
-    const [tags, setTags] = useState(new Map<string, TagModel>());
-    const modalOnEventProps: Project | undefined = useContext(modalOnEventContext);
+    const modalOnEventProps = useContext(modalOnEventContext);
 
-    const onOwnerChange = useCallback(setOwner, [setOwner]);
-    const onProjectChange = useCallback(setProject, [setProject]);
-    const onStateChange = useCallback(setState, [setState]);
-    const onPriorityChange = useCallback(setPriority, [setPriority]);
-    const onEstimateChange = useCallback(setEstimate, [setEstimate]);
-    const onTagAdd = useCallback(
-        (tag: TagModel) => {
-            const newTags = new Map(tags);
-            newTags.set(tag.id, tag);
-            setTags(newTags);
-        },
-        [tags],
-    );
-    const onTagDelete = useCallback(
-        (tag: TagModel) => {
-            const newTags = new Map(tags);
-            newTags.delete(tag.id);
-            setTags(newTags);
-        },
-        [tags],
-    );
-
-    useEffect(() => {
-        const defaultState = project?.flow?.states?.filter((s) => s?.default)[0];
-        if (defaultState) {
-            setState(defaultState);
-        }
-    }, [project]);
-
-    useEffect(() => {
-        if (modalOnEventProps) {
-            setProject(modalOnEventProps);
-        }
-    }, [modalOnEventProps]);
-
-    const createGoal = async ({ title, description }: GoalFormType) => {
-        if (!owner.id || !project?.id) return;
-
+    const createGoal = async (form: GoalFormType) => {
         const promise = gql.mutation({
             createGoal: [
                 {
                     data: {
-                        title,
-                        description,
-                        ownerId: owner.id,
-                        projectId: project.id,
-                        estimate,
-                        stateId: state?.id,
-                        priority,
-                        tags: Array.from(tags.values()),
+                        title: form.title,
+                        description: form.description,
+                        ownerId: form.owner.id,
+                        projectId: form.project.id,
+                        stateId: form.state.id,
+                        priority: form.priority,
+                        tags: form.tags,
+                        estimate: form.estimate,
                     },
                 },
                 {
@@ -116,20 +73,10 @@ const GoalCreateForm: React.FC<GoalCreateFormProps> = ({ locale, onCreate }) => 
             i18nKeyset="goals.new"
             formTitle={t('Create new goal')}
             locale={locale}
-            owner={owner!}
-            project={project}
-            state={state}
+            owner={{ id: session?.user.activityId, user: session?.user } as Partial<Activity>}
+            project={modalOnEventProps}
             priority="Medium"
-            tags={tags}
-            estimate={estimate}
             onSumbit={createGoal}
-            onOwnerChange={onOwnerChange}
-            onProjectChange={onProjectChange}
-            onEstimateChange={onEstimateChange}
-            onStateChange={onStateChange}
-            onPriorityChange={onPriorityChange}
-            onTagAdd={onTagAdd}
-            onTagDelete={onTagDelete}
         >
             <StyledFormBottom>
                 <Tip title={t('Pro tip!')} icon={<Icon type="bulbOn" size="s" color={star0} />}>
