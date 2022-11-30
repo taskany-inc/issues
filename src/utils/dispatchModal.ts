00f2936cfd37a75e9fe93538a41734c9701ea5fa
Tony Vi <winogradovaa@gmail.com>
2022-11-30 22:36:28 +0200

    remove CustomEvent props from ModalOnEvent

--- src/utils/dispatchModal.ts
+++ src/utils/dispatchModal.ts
@@ -8,7 +8,6 @@ export enum ModalEvent {
     UserInviteModal = 'UserInviteModal',
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const dispatchModalEvent = (e: ModalEvent, props?: any) => () => {
-    window.dispatchEvent(new CustomEvent(e, { detail: props }));
+export const dispatchModalEvent = (e: ModalEvent) => () => {
+    window.dispatchEvent(new Event(e));
 };
