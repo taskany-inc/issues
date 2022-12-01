2d5dd6449ff76a8caf7dfb5e6a624f1b727ab96f
Tony Vi <winogradovaa@gmail.com>
2022-12-01 22:57:18 +0200

    revalidate ssrTime on mount

--- src/components/RelativeTime.tsx
+++ src/components/RelativeTime.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { useTranslations } from 'next-intl';
 
 import { dateAgo, createLocaleDate, parseLocaleDate } from '../utils/dateTime';
 import { usePageContext } from '../hooks/usePageContext';
+import { useMounted } from '../hooks/useMounted';
 
 import { Light } from './Light';
 
@@ -13,14 +15,24 @@ interface RelativeTimeProps {
 const RelativeTime: React.FC<RelativeTimeProps> = ({ kind, date }) => {
     const t = useTranslations('RelativeTime');
     const { locale, ssrTime } = usePageContext();
+    const [time, setTime] = useState(ssrTime);
+    const mounted = useMounted(0);
     const localeDate = parseLocaleDate(date, { locale });
+
+    useEffect(() => {
+        setTime(Date.now());
+    }, [mounted]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTime(Date.now());
+        }, 100); // TODO: use a better interval?
+    }, []);
 
     return (
         <>
             {kind ? `${t(kind)} ` : ''}
-            <Light title={createLocaleDate(localeDate, { locale })}>
-                {dateAgo(localeDate, ssrTime || Date.now(), { locale })}
-            </Light>
+            <Light title={createLocaleDate(localeDate, { locale })}>{dateAgo(localeDate, time, { locale })}</Light>
         </>
     );
 };
