f032829622fc44cfbddf90c3465734197229948b
Tony Vi <winogradovaa@gmail.com>
2022-12-13 23:33:04 +0200

    disable Sentry in dev env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,12 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (SENTRY_DSN) {
-    Sentry.init({
-        dsn: SENTRY_DSN,
-        tracesSampleRate: 1.0,
-    });
+    if (process.env.NODE_ENV === 'production') {
+        Sentry.init({
+            dsn: SENTRY_DSN,
+            tracesSampleRate: 1.0,
+        });
+    }
 }
 
 const App = ({ Component, pageProps }: AppProps) => {
