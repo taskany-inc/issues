d306eeef7704b9b230f9ce73e65662e51721b189
Tony Vi <winogradovaa@gmail.com>
2022-12-24 00:11:22 +0200

    feat: universal WatchButton and StarButton

---
+++ src/components/StarButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useTranslations } from 'next-intl';
+
+import { Button } from './Button';
+import { Icon } from './Icon';
+import { Badge } from './Badge';
+
+interface StarButtonProps {
+    stargizer?: boolean;
+    count?: number;
+
+    onToggle: () => void;
+}
+
+export const StarButton: React.FC<StarButtonProps> = ({ stargizer, count, onToggle }) => {
+    const t = useTranslations('StarButton');
+
+    return (
+        <Button
+            text={t(stargizer ? 'Starred' : 'Stars')}
+            iconLeft={<Icon noWrap type={stargizer ? 'starFilled' : 'star'} size="s" />}
+            iconRight={count ? <Badge>{count}</Badge> : undefined}
+            onClick={onToggle}
+        />
+    );
+};
---
+++ src/components/WatchButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useTranslations } from 'next-intl';
+
+import { Button } from './Button';
+import { Icon } from './Icon';
+
+interface WatchButtonProps {
+    watcher?: boolean;
+
+    onToggle: () => void;
+}
+
+export const WatchButton: React.FC<WatchButtonProps> = ({ watcher, onToggle }) => {
+    const t = useTranslations('WatchButton');
+
+    return (
+        <Button
+            text={t(watcher ? 'Watching' : 'Watch')}
+            iconLeft={<Icon noWrap type={watcher ? 'eye' : 'eyeClosed'} size="s" />}
+            onClick={onToggle}
+        />
+    );
+};
