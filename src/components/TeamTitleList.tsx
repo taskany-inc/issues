d0ac1af6488209e4260a848fe248ba6b037080c6
Tony Vi <winogradovaa@gmail.com>
2023-03-03 22:35:58 +0200

    chore(TeamTitleList): teams list renderer

---
+++ src/components/TeamTitleList.tsx
@@ -0,0 +1,24 @@
+import NextLink from 'next/link';
+
+import { Team } from '../../graphql/@generated/genql';
+import { nullable } from '../utils/nullable';
+import { routes } from '../hooks/router';
+
+import { Link } from './Link';
+
+export const TeamTitleList = ({ teams = [] }: { teams: Team[] }) => (
+    <>
+        {teams.map((team, i) =>
+            nullable(team, (te) => (
+                <span key={te.title}>
+                    <NextLink key={te.slug} passHref href={routes.team(te.slug)}>
+                        <Link inline title={te.description}>
+                            {te.title}
+                        </Link>
+                    </NextLink>
+                    {i < teams.length - 1 ? ', ' : ''}
+                </span>
+            )),
+        )}
+    </>
+);
