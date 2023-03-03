4ea5c206d8071dcb0231cc015dccd66174b5fef5
Tony Vi <winogradovaa@gmail.com>
2023-03-03 23:17:13 +0200

    chore(prisma): one shared prismaClient

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,35 +1,9 @@
 import { PrismaClient, User, Activity } from '@prisma/client';
 import { NextApiRequest } from 'next';
 
-const db = new PrismaClient();
-
-db.$use(async (params, next) => {
-    if (params.model === 'User') {
-        if (params.action === 'create') {
-            const newUserEmail = params.args.data.email;
-            const connectedGhost = await db.ghost.findUnique({
-                where: { email: newUserEmail },
-                include: { activity: true },
-            });
-
-            params.args.data.hostId = connectedGhost?.hostId;
-            params.args.data.activityId = connectedGhost?.activity?.id;
-            params.args.data.invitedAt = connectedGhost?.createdAt;
-
-            await db.ghost.delete({ where: { id: connectedGhost?.id } });
-        }
-    }
-
-    return next(params);
-});
-
 export type Context = {
     db: PrismaClient;
     req: NextApiRequest;
     user: User | null;
     activity: Activity | null;
-};
-
-export const context = {
-    db,
 };
