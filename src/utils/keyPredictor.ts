df9d69ac65d45c944cbb37cea24bda484d6f6dad
Tony Vi <winogradovaa@gmail.com>
2023-03-04 22:40:52 +0200

    chore(keyPredictor): allow numbers

--- src/utils/keyPredictor.ts
+++ src/utils/keyPredictor.ts
@@ -1,7 +1,7 @@
 import slugify from 'slugify';
 
 export const keyPredictor = (str: string) =>
-    slugify(str.replace(/[0-9]/g, ''), {
+    slugify(str, {
         // eslint-disable-next-line no-useless-escape
         remove: /[aeiou `~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]|/gi,
     })
