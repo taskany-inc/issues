03606a0cfdd511db6c6fda7af6e0ec7ea564b6ef
Tony Vi <winogradovaa@gmail.com>
2023-03-10 13:59:50 +0200

    chore(keyPredictor): remove slugify usage

--- src/utils/keyPredictor.ts
+++ src/utils/keyPredictor.ts
@@ -1,11 +1,9 @@
-import slugify from 'slugify';
-
 export const keyPredictor = (str: string) =>
-    // eslint-disable-next-line no-control-regex
-    slugify(str.trim().replace(/[^\x00-\x7F]/g, ''), {
+    str
+        .trim()
+        // eslint-disable-next-line no-control-regex
+        .replace(/[^\x00-\x7F]/g, '')
         // eslint-disable-next-line no-useless-escape
-        remove: /[aeiou `~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]|/gi,
-    })
+        .replace(/[aeiou `~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]|/gi, '')
         .toUpperCase()
-        // For the key to consist of 10 characters
         .slice(0, 10);
