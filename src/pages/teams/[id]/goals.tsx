57c436053aee1a04374aae659dc4771075e186ec
Tony Vi <winogradovaa@gmail.com>
2023-04-12 19:46:08 +0300

    chore!: drop useless routes

--- src/pages/teams/[id]/goals.tsx
+++
@@ -1,5 +0,0 @@
-import { TeamGoalsPage } from '../../../components/pages/TeamGoalsPage/TeamGoalsPage';
-
-export { getServerSideProps } from '../../../components/pages/TeamGoalsPage/TeamGoalsPage';
-
-export default TeamGoalsPage;
--- src/pages/users/[id].tsx
+++
@@ -1,5 +0,0 @@
-import { UserPage } from '../../components/pages/UserPage/UserPage';
-
-export { getServerSideProps } from '../../components/pages/UserPage/UserPage';
-
-export default UserPage;
