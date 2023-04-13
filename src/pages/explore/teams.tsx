780901fdb6f5836f45a3dfc8ba6d1c8c505683ee
Tony Vi <winogradovaa@gmail.com>
2023-04-13 16:51:46 +0300

    chore!: drop other part of teams components

--- src/pages/explore/teams.tsx
+++
@@ -1,5 +0,0 @@
-import { ExploreTeamsPage } from '../../components/pages/ExploreTeamsPage/ExploreTeamsPage';
-
-export { getServerSideProps } from '../../components/pages/ExploreTeamsPage/ExploreTeamsPage';
-
-export default ExploreTeamsPage;
