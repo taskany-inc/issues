74906323e97d5a4b08583bb01335e54b6e5c9dc6
Tony Vi <winogradovaa@gmail.com>
2023-04-20 11:29:15 +0300

    feat: add PresetFilterDropdown

---
+++ src/components/PresetFilterDropdown.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from 'react';
+import { Dropdown, MenuItem } from '@taskany/bricks';
+
+import { Filter } from '../../graphql/@generated/genql';
+
+import { FiltersMenuItem } from './FiltersMenuItem';
+
+interface PresetFilterDropdownProps {
+    text: React.ComponentProps<typeof Dropdown>['text'];
+    presets: Filter[];
+    value?: string;
+    disabled?: React.ComponentProps<typeof Dropdown>['disabled'];
+
+    onChange?: (selected: string | undefined) => void;
+}
+
+export const PresetFilterDropdown: React.FC<PresetFilterDropdownProps> = React.forwardRef<
+    HTMLDivElement,
+    PresetFilterDropdownProps
+>(({ text, presets, value, disabled, onChange }, ref) => {
+    const onPresetClick = useCallback(
+        (p: Filter) => {
+            const newSelected = value === p.id ? undefined : p.id;
+
+            onChange?.(newSelected);
+        },
+        [onChange, value],
+    );
+
+    return (
+        <Dropdown
+            ref={ref}
+            text={text}
+            value={value}
+            onChange={onPresetClick}
+            items={presets}
+            disabled={disabled}
+            renderTrigger={(props) => (
+                <FiltersMenuItem
+                    ref={props.ref}
+                    active={Boolean(value)}
+                    disabled={props.disabled}
+                    onClick={props.onClick}
+                >
+                    {props.text}
+                </FiltersMenuItem>
+            )}
+            renderItem={(props) => (
+                <MenuItem ghost key={props.item.id} selected={value === props.item.id} onClick={props.onClick}>
+                    {props.item.title}
+                </MenuItem>
+            )}
+        />
+    );
+});
