ff51e2f9653f7e37c42fad01af424141ca9b2d59
Tony Vi <winogradovaa@gmail.com>
2023-04-24 17:45:36 +0300

    feat(Filter): star/unstar not owned filters

--- graphql/resolvers/Filter.ts
+++ graphql/resolvers/Filter.ts
@@ -1,7 +1,8 @@
 import { arg, nonNull } from 'nexus';
 import { ObjectDefinitionBlock } from 'nexus/dist/core';
 
-import { Filter, FilterCreateInput, FilterInput } from '../types';
+import { Filter, FilterCreateInput, FilterInput, Activity, SubscriptionToggleInput } from '../types';
+import { connectionMap } from '../queries/connections';
 
 export const query = (t: ObjectDefinitionBlock<'Query'>) => {
     t.field('filter', {
@@ -12,15 +13,22 @@ export const query = (t: ObjectDefinitionBlock<'Query'>) => {
         resolve: async (_, { data }, { db, activity }) => {
             if (!activity) return null;
 
-            try {
-                return db.filter.findUnique({
-                    where: {
-                        id: data.id,
-                    },
-                });
-            } catch (error) {
-                throw Error(`${error}`);
-            }
+            const filter = await db.filter.findUnique({
+                where: {
+                    id: data.id,
+                },
+                include: {
+                    stargizers: true,
+                },
+            });
+
+            if (!filter) return null;
+
+            return {
+                ...filter,
+                _isOwner: filter?.activityId === activity.id,
+                _isStarred: filter?.stargizers?.some((stargizer) => stargizer?.id === activity.id),
+            };
         },
     });
 };
@@ -41,6 +49,37 @@ export const mutation = (t: ObjectDefinitionBlock<'Mutation'>) => {
                         activityId: activity.id,
                     },
                 });
+            } catch (error) {
+                throw Error(`${error}`);
+            }
+        },
+    });
+
+    t.field('toggleFilterStargizer', {
+        type: Activity,
+        args: {
+            data: nonNull(arg({ type: SubscriptionToggleInput })),
+        },
+        resolve: async (_, { data: { id, direction } }, { db, activity }) => {
+            if (!activity) return null;
+
+            const connection = { id };
+
+            try {
+                return db.activity.update({
+                    where: { id: activity.id },
+                    data: {
+                        filterStargizers: { [connectionMap[String(direction)]]: connection },
+                    },
+                });
+
+                // await mailServer.sendMail({
+                //     from: `"Fred Foo 👻" <${process.env.MAIL_USER}>`,
+                //     to: 'bar@example.com, baz@example.com',
+                //     subject: 'Hello ✔',
+                //     text: `new post '${title}'`,
+                //     html: `new post <b>${title}</b>`,
+                // });
             } catch (error) {
                 throw Error(`${error}`);
             }
