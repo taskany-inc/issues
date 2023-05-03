aa4a4d8b47e425b1ceee7b7fc59add755a618813
tsumo <tsumo@users.noreply.github.com>
2023-05-03 15:42:42 +0300

    fix(GoalsPage): hook call without condition

--- src/components/GoalsPage/GoalsPage.tsx
+++ src/components/GoalsPage/GoalsPage.tsx
@@ -152,9 +152,7 @@ export const GoalsPage = ({ user, ssrTime, locale, fallback }: ExternalPageProps
 
     const utils = trpc.useContext();
 
-    const presetData = router.query.filter
-        ? trpc.filter.getById.useQuery(router.query.filter as string)
-        : { data: undefined };
+    const presetData = trpc.filter.getById.useQuery(router.query.filter as string, { enabled: !!router.query.filter });
 
     const {
         currentPreset,
