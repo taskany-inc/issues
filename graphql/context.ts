1b1716f170a51e2fe19d6ab3b0277ec796a5f74a
Tony Vi <winogradovaa@gmail.com>
2023-05-07 19:25:02 +0300

    chore: remove nexus, gql, swr

--- graphql/context.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient, User, Activity } from '@prisma/client';
-import { NextApiRequest } from 'next';
-
-export type Context = {
-    db: PrismaClient;
-    req: NextApiRequest;
-    user: User | null;
-    activity: Activity | null;
-};
--- src/utils/gql.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '../../graphql/@generated/genql';
-
-export const gql = createClient({
-    url: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/graphql`,
-});
