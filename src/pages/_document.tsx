557e5cb288b549c8abad4918823bc4949b2f9bab
Tony Vi <winogradovaa@gmail.com>
2023-05-18 00:46:48 +0300

    feat: react and react-dom are externals by default

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,34 @@
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
+const ExternalScripts = process.env.INCLUDE_SCRIPTS_TO_MAIN_BUNDLE
+    ? () => null
+    : () => (
+          <>
+              <Script strategy="beforeInteractive" src="https://unpkg.com/react@18/umd/react.production.js"></Script>
+              <Script
+                  strategy="beforeInteractive"
+                  src="https://unpkg.com/react-dom@18/umd/react-dom.production.js"
+              ></Script>
+          </>
+      );
+
 export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <ExternalScripts />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+
     static async getInitialProps(ctx: DocumentContext) {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
