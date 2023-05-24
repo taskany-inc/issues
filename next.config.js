b3e13956d18eab5343919ea92ab2419670c15aa7
Tony Vi <winogradovaa@gmail.com>
2023-05-24 22:31:39 +0300

    chore(wdyr): move to utils

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
         if (dev && !isServer) {
             const originalEntry = config.entry;
             config.entry = async () => {
-                const wdrPath = path.resolve(__dirname, './scripts/wdyr.ts');
+                const wdrPath = path.resolve(__dirname, './src/utils/wdyr.ts');
                 const entries = await originalEntry();
 
                 if (entries['main.js'] && !entries['main.js'].includes(wdrPath)) {
