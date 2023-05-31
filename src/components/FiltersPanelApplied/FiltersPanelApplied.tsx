60fb1495e16c6aae4b00bc42205bab15b1a1fbc0
Tony Vi <winogradovaa@gmail.com>
2023-05-31 13:55:15 +0300

    feat(FiltersPanelApplied): show applied sort props

--- src/components/FiltersPanelApplied/FiltersPanelApplied.tsx
+++ src/components/FiltersPanelApplied/FiltersPanelApplied.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
-import { FiltersApplied, Text } from '@taskany/bricks';
-import { gapM, gray7 } from '@taskany/colors';
+import { FiltersApplied } from '@taskany/bricks';
+import { gray7 } from '@taskany/colors';
 
 import { QueryState } from '../../hooks/useUrlFilterParams';
 import { getPriorityText } from '../PriorityText/PriorityText';
@@ -10,6 +10,7 @@ import { UserFilter } from '../UserFilter';
 import { ProjectFilter } from '../ProjectFilter';
 import { TagFilter } from '../TagFilter';
 import { EstimateFilter } from '../EstimateFilter';
+import { SortableProps, sortFilterTr } from '../SortFilter/SortFilter';
 
 import { tr } from './FiltersPanelApplied.i18n';
 
@@ -43,9 +44,6 @@ export const FiltersPanelApplied: React.FC<FiltersPanelAppliedProps> = ({
 }) => {
     let infoString = '';
 
-    // TODO: https://github.com/taskany-inc/issues/issues/805
-    // NB: it is reason why optional chaining in maps below.
-
     const { statesMap, ownersMap, projectsMap, tagsMap } = useMemo(() => {
         return {
             statesMap: arrToMap(states || []),
@@ -62,19 +60,19 @@ export const FiltersPanelApplied: React.FC<FiltersPanelAppliedProps> = ({
     }
 
     if (queryState.state.length && states?.length) {
-        appliedMap[tr('State')] = queryState.state.map((s) => statesMap[s]?.title).filter(Boolean);
+        appliedMap[tr('State')] = queryState.state.map((s) => statesMap[s].title).filter(Boolean);
     }
 
     if (queryState.owner.length && users?.length) {
-        appliedMap[tr('Owner')] = queryState.owner.map((u) => ownersMap[u]?.user?.name).filter(Boolean) as string[];
+        appliedMap[tr('Owner')] = queryState.owner.map((u) => ownersMap[u].user?.name).filter(Boolean) as string[];
     }
 
     if (queryState.project.length && projects?.length) {
-        appliedMap[tr('Project')] = queryState.project.map((p) => projectsMap[p]?.title).filter(Boolean);
+        appliedMap[tr('Project')] = queryState.project.map((p) => projectsMap[p].title).filter(Boolean);
     }
 
     if (queryState.tag.length && tags?.length) {
-        appliedMap[tr('Tag')] = queryState.tag.map((t) => tagsMap[t]?.title).filter(Boolean);
+        appliedMap[tr('Tag')] = queryState.tag.map((t) => tagsMap[t].title).filter(Boolean);
     }
 
     if (queryState.estimate.length && estimates?.length) {
@@ -84,6 +82,16 @@ export const FiltersPanelApplied: React.FC<FiltersPanelAppliedProps> = ({
     Object.entries(appliedMap).forEach(([k, v]) => {
         if (v.length) infoString += `${k}: ${v.join(', ')}. `;
     });
+
+    if (queryState.sort) {
+        infoString += `${tr('Sorted')}: `;
+        const items = Object.entries(queryState.sort);
+
+        items.forEach(([k, v], i) => {
+            const delimeter = items.length - 1 === i ? '.' : ', ';
+            if (v.length) infoString += `${sortFilterTr(k as SortableProps)}(${v})${delimeter}`;
+        });
+    }
 
     return (
         <FiltersApplied size="s" weight="bold" color={gray7}>
