44c3cf6ed30513abdd391a13af494c27fc240798
Tony Vi <winogradovaa@gmail.com>
2023-06-07 16:44:46 +0300

    feat(PageHeader): add GlobalSearch

---
+++ src/components/ProjectListItemCompact.tsx
@@ -0,0 +1,53 @@
+import React, { MouseEventHandler } from 'react';
+import NextLink from 'next/link';
+import { ProjectIcon } from '@taskany/bricks';
+
+import { routes } from '../hooks/router';
+import { ActivityByIdReturnType } from '../../trpc/inferredTypes';
+
+import { UserGroup } from './UserGroup';
+import { TableRow, ContentItem, TitleItem, TitleContainer, Title, TextItem } from './Table';
+
+interface ProjectListItemCompactProps {
+    id: string;
+    title: string;
+    owner?: ActivityByIdReturnType;
+    focused?: boolean;
+    className?: string;
+    onClick?: MouseEventHandler<HTMLAnchorElement>;
+}
+
+export const ProjectListItemCompact: React.FC<ProjectListItemCompactProps> = React.memo(
+    ({ id, owner, title, focused, className, onClick }) => {
+        return (
+            <NextLink href={routes.project(id)} passHref>
+                <TableRow focused={focused} onClick={onClick} className={className}>
+                    <ContentItem>
+                        <ProjectIcon size="s" />
+                    </ContentItem>
+
+                    <TitleItem>
+                        <TitleContainer>
+                            <Title size="s" weight="bold">
+                                {title}
+                            </Title>
+                        </TitleContainer>
+                    </TitleItem>
+
+                    <ContentItem />
+                    <ContentItem />
+
+                    <ContentItem>
+                        <TextItem>{id}</TextItem>
+                    </ContentItem>
+
+                    <ContentItem align="center">
+                        <UserGroup users={[owner] as NonNullable<ActivityByIdReturnType>[]} />
+                    </ContentItem>
+
+                    <ContentItem />
+                </TableRow>
+            </NextLink>
+        );
+    },
+);
