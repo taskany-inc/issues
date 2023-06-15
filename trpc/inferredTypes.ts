b98a2c9c85a2de07d79b5cb2c9ce9c85820f5026
Maksim Sviridov <enemyworld@gmail.com>
2023-06-15 17:41:29 +0300

    fix(GoalAchievement): used components improvements

--- trpc/inferredTypes.ts
+++ trpc/inferredTypes.ts
@@ -9,3 +9,5 @@ export type ProjectByIdReturnType = RouterOutputs['project']['getById'];
 export type ProjectUpdateReturnType = RouterOutputs['project']['update'];
 export type GoalByIdReturnType = RouterOutputs['goal']['getById'];
 export type ActivityByIdReturnType = RouterOutputs['user']['suggestions'][number];
+export type GoalEstimate = NonNullable<GoalByIdReturnType>['_lastEstimate'];
+export type GoalAchiveCriteria = NonNullable<GoalByIdReturnType>['goalAchiveCriteria'][number];
