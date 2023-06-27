78ce63bf032aaf1b6a36b6db46cf0d058aa55261
Tony Vi <winogradovaa@gmail.com>
2023-06-27 18:00:05 +0300

    fix(GoalsPage): sync client and server fetch

--- src/pages/goals/index.tsx
+++ src/pages/goals/index.tsx
@@ -1,18 +1,20 @@
 import { GoalsPage } from '../../components/GoalsPage/GoalsPage';
 import { declareSsrProps } from '../../utils/declareSsrProps';
-// import { parseFilterValues } from '../../hooks/useUrlFilterParams';
+import { parseFilterValues } from '../../hooks/useUrlFilterParams';
+
+const pageSize = 20;
 
 export const getServerSideProps = declareSsrProps(
     async ({ query, ssrHelpers }) => {
-        // const preset =
-        typeof query.filter === 'string' ? await ssrHelpers.filter.getById.fetch(query.filter) : undefined;
+        const preset =
+            typeof query.filter === 'string' ? await ssrHelpers.filter.getById.fetch(query.filter) : undefined;
 
-        await ssrHelpers.goal.getBatch.fetchInfinite({ limit: 20 });
+        await ssrHelpers.goal.getBatch.fetchInfinite({
+            limit: pageSize,
+            query: parseFilterValues(preset ? Object.fromEntries(new URLSearchParams(preset.params)) : query),
+        });
 
-        // await ssrHelpers.filter.getUserFilters.fetch();
-        // await ssrHelpers.goal.getUserGoals.fetch(
-        //     parseFilterValues(preset ? Object.fromEntries(new URLSearchParams(preset.params)) : query),
-        // );
+        await ssrHelpers.filter.getUserFilters.fetch();
     },
     {
         private: true,
