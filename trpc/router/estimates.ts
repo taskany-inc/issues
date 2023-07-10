0a1a6f5aeb09ad8e7e7911db01bf42f468db42cd
Timur Minulin <timur@minulin.in>
2023-07-10 18:07:43 +0300

    fix(estimateFilter): only unique estimates

--- trpc/router/estimates.ts
+++ trpc/router/estimates.ts
@@ -2,5 +2,12 @@ import { prisma } from '../../src/utils/prisma';
 import { protectedProcedure, router } from '../trpcBackend';
 
 export const estimates = router({
-    all: protectedProcedure.query(async () => prisma.estimate.findMany()),
+    all: protectedProcedure.query(async () =>
+        prisma.estimate.groupBy({
+            by: ['q', 'y'],
+            orderBy: {
+                y: 'desc',
+            },
+        }),
+    ),
 });
