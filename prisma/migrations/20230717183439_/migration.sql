b636afa8bfff88b803c4f85f79006db1bff11bb7
Tony Vi <winogradovaa@gmail.com>
2023-07-18 18:05:46 +0300

    fix(prisma): resolve migrations

--- prisma/migrations/20230717183439_/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Goal" DROP CONSTRAINT "Goal_priorityId_fkey";
-
--- AlterTable
-ALTER TABLE "Goal" ALTER COLUMN "priority" DROP NOT NULL,
-ALTER COLUMN "priorityId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Goal" ADD CONSTRAINT "Goal_priorityId_fkey" FOREIGN KEY ("priorityId") REFERENCES "Priority"("id") ON DELETE SET NULL ON UPDATE CASCADE;
