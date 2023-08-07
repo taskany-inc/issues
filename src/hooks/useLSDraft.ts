aeb27d912e1d4302ac391536e2f4cce4f3fd6748
Tony Vi <winogradovaa@gmail.com>
2023-08-07 23:15:36 +0300

    chore(useLSDraft): simplify types

--- src/hooks/useLSDraft.ts
+++ src/hooks/useLSDraft.ts
@@ -6,43 +6,21 @@ type LSParams = Parameters<typeof useLocalStorage>;
 
 type DraftComment = DraftGoalComment[keyof DraftGoalComment];
 type DraftGoalCommentKey = 'draftGoalComment';
-type DraftGoalCommentKeyReturn = {
-    saveDraft: (id: string, draft: DraftComment) => void;
-    resolveDraft: (id: string) => DraftComment;
-    removeDraft: (id: string) => void;
-};
 
-function useLSDraft(
-    storageKey: DraftGoalCommentKey,
-    initialValue: Record<string, DraftComment>,
-): DraftGoalCommentKeyReturn;
-
-/**
- * before using, you need to set up an overload
- */
-function useLSDraft<KDG extends DraftGoalCommentKey>(
-    storageKey: KDG,
-    initialValue: Record<string, LSParams[1]>,
-): DraftGoalCommentKeyReturn {
+function useLSDraft<KDG extends DraftGoalCommentKey>(storageKey: KDG, initialValue: Record<string, LSParams[1]>) {
     const [draft, setDraft] = useLocalStorage(storageKey, initialValue);
 
     const saveDraft = useCallback(
         (id: string, draft: DraftComment) => {
-            setDraft((prev) => {
-                // @ts-expect-error
-                prev[id] = draft;
-                return prev;
-            });
+            setDraft((prev) => ({
+                ...prev,
+                [id]: draft,
+            }));
         },
         [setDraft],
     );
 
-    const resolveDraft = useCallback(
-        (id: string) => {
-            return draft[id];
-        },
-        [draft],
-    );
+    const resolveDraft = useCallback((id?: string) => (id ? draft[id] : undefined), [draft]);
 
     const removeDraft = useCallback(
         (id: string) => {
