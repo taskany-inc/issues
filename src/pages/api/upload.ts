bc8fc4338663ca8b71016e6f1b330e3ff9e79ce8
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2023-08-08 13:54:34 +0300

    feat(upload): upload attaches with error handling

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable no-await-in-loop */
 import nextConnect from 'next-connect';
 import crypto from 'crypto';
 import multer from 'multer';
@@ -22,6 +23,12 @@ const cors = Cors({
     origin: false,
     methods: ['HEAD', 'OPTIONS', 'POST'],
 });
+
+type ResponseObj = {
+    failed: string[];
+    succeeded: string[];
+    errorMessage?: string;
+};
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -55,43 +62,48 @@ route.post(upload.array(formFieldName, 10), async (req: any, res: NextApiRespons
             region: process.env.S3_REGION,
         });
     }
+    const resultObject: ResponseObj = {
+        failed: [],
+        succeeded: [],
+    };
 
-    const result = await Promise.all(
-        req[formFieldName].map(
-            async ({ originalname, mimetype, buffer }: { originalname: string; mimetype: string; buffer: Buffer }) => {
-                // @ts-ignore Translit is any ¯\_(ツ)_/¯
-                let fileName = new Translit({ preset: 'ru' }).transform(originalname, '_');
+    for (const file of req[formFieldName]) {
+        const { originalname, mimetype, buffer } = file;
+        // @ts-ignore Translit is any ¯\_(ツ)_/¯
+        let fileName = new Translit({ preset: 'ru' }).transform(originalname, '_');
 
-                if (JSON.parse(uniqueNames)) {
-                    const hashSum = crypto.createHash('sha256');
-                    hashSum.update(buffer);
-                    const hex = hashSum.digest('hex');
+        if (JSON.parse(uniqueNames)) {
+            const hashSum = crypto.createHash('sha256');
+            hashSum.update(buffer);
+            const hex = hashSum.digest('hex');
 
-                    fileName = `${hex.slice(0, 8)}_${fileName}`;
-                }
+            fileName = `${hex.slice(0, 8)}_${fileName}`;
+        }
 
-                if (!s3Client) {
-                    return saveFileInPublic(fileName, buffer);
-                }
-
-                return Promise.resolve(
-                    s3Client.send(
-                        new PutObjectCommand({
-                            Bucket: process.env.S3_BUCKET,
-                            BucketKeyEnabled: false,
-                            Key: `${folder}/${fileName}`,
-                            Body: buffer,
-                            ACL: 'authenticated-read',
-                            CacheControl: 'max-age=630720000, public',
-                            ContentType: mimetype,
-                        }),
-                    ),
-                ).then(() => `${process.env.PUBLIC_URL}/static/${folder}/${fileName}`);
-            },
-        ),
-    );
-
-    res.status(200).json(result);
+        if (!s3Client) {
+            const filePath = await saveFileInPublic(fileName, buffer);
+            resultObject.succeeded.push(filePath);
+        } else {
+            await s3Client
+                .send(
+                    new PutObjectCommand({
+                        Bucket: process.env.S3_BUCKET,
+                        BucketKeyEnabled: false,
+                        Key: `${folder}/${fileName}`,
+                        Body: buffer,
+                        ACL: 'authenticated-read',
+                        CacheControl: 'max-age=630720000, public',
+                        ContentType: mimetype,
+                    }),
+                )
+                .then(() => resultObject.succeeded.push(`${process.env.PUBLIC_URL}/static/${folder}/${fileName}`))
+                .catch((error) => {
+                    resultObject.failed.push(originalname);
+                    resultObject.errorMessage = error.message;
+                });
+        }
+    }
+    res.status(200).json(resultObject);
 });
 
 export default route;
