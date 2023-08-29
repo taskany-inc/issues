450a6fddfc1f9ca9fa48ba4c9f25caad2660def2
troff8 <troffibp@gmail.com>
2023-08-29 14:04:20 +0400

    feat(GoalPage): show goal tags in sidebar

--- src/components/TagComboBox.tsx
+++ src/components/TagComboBox.tsx
@@ -22,6 +22,7 @@ interface TagComboBoxProps {
     error?: React.ComponentProps<typeof ComboBox>['error'];
 
     onChange?: (value: TagObject[]) => void;
+    renderTrigger?: React.ComponentProps<typeof ComboBox>['renderTrigger'];
 }
 
 const StyledInput = styled(Input)`
@@ -29,14 +30,21 @@ const StyledInput = styled(Input)`
 `;
 
 export const TagComboBox = React.forwardRef<HTMLDivElement, TagComboBoxProps>(
-    ({ text, value = [], query = '', error, disabled, placeholder, onChange }, ref) => {
+    ({ text, value = [], query = '', error, disabled, placeholder, onChange, renderTrigger }, ref) => {
         const [completionVisible, setCompletionVisibility] = useState(false);
         const [inputState, setInputState] = useState(query);
         const [tags, setTags] = useState(value);
 
-        const suggestions = trpc.tag.suggestions.useQuery({
-            query: inputState,
-        });
+        const suggestions = trpc.tag.suggestions.useQuery(
+            {
+                query: inputState,
+            },
+            {
+                enabled: inputState.length >= 3,
+                cacheTime: 0,
+                staleTime: 0,
+            },
+        );
         const createMutation = trpc.tag.create.useMutation();
 
         const createTag = useCallback(async () => {
@@ -92,14 +100,18 @@ export const TagComboBox = React.forwardRef<HTMLDivElement, TagComboBoxProps>(
                 onClickOutside={onClickOutside}
                 onChange={onTagClick}
                 items={items}
-                renderTrigger={(props) => (
-                    <Button
-                        text={props.text}
-                        disabled={props.disabled}
-                        onClick={props.onClick}
-                        iconLeft={<IconTagOutline noWrap size="xs" />}
-                    />
-                )}
+                renderTrigger={(props) =>
+                    renderTrigger ? (
+                        renderTrigger(props)
+                    ) : (
+                        <Button
+                            text={props.text}
+                            disabled={props.disabled}
+                            onClick={props.onClick}
+                            iconLeft={<IconTagOutline noWrap size="xs" />}
+                        />
+                    )
+                }
                 renderInput={(props) => (
                     <StyledInput
                         autoFocus
