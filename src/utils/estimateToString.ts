f738e76389392c7206c74e446df185e3dbbb3cde
Anton <sab94a@yandex.ru>
2023-08-30 15:29:17 +0300

    fix(FiltersPanel): estimate as JSON at search URL

--- src/utils/estimateToString.ts
+++ src/utils/estimateToString.ts
@@ -1,8 +1,25 @@
-import { Estimate } from '@prisma/client';
+import { Estimate as EstimateType } from '@prisma/client';
 
-export const estimateToString = (estimate: { q: Estimate['q']; y: Estimate['y'] }) => {
+export type Estimate = { q: EstimateType['q']; y: EstimateType['y'] };
+
+export const estimateToString = (estimate: Estimate) => {
     if (!estimate.q) {
         return estimate.y;
     }
     return `${estimate.q}/${estimate.y}`;
+};
+
+export const encodeEstimateFilterValue = ({ y, q }: Estimate) => encodeURIComponent(JSON.stringify({ q, y }));
+export const decodeEstimateFilterValue = (data: string): null | Estimate => {
+    try {
+        const { q = null, y } = JSON.parse(decodeURIComponent(data));
+
+        if (y) {
+            return { q, y };
+        }
+
+        return null;
+    } catch (e) {
+        return null;
+    }
 };
