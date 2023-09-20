d762c91ee0c74c6a6b7c02d50574d5e6c2d2145c
Maksim Sviridov <enemyworld@gmail.com>
2023-09-20 18:04:46 +0300

    fix(CreateGoal): can create goal for empty project

---
+++ src/components/InlineCreateGoalControl/InlineCreateGoalControl.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { gapSm } from '@taskany/colors';
+import { IconPlusCircleOutline } from '@taskany/icons';
+
+import { InlineTrigger } from '../InlineTrigger';
+import { ModalEvent, dispatchModalEvent } from '../../utils/dispatchModal';
+import { createGoalInlineControl } from '../../utils/domObjects';
+
+import { tr } from './InlineCreateGoalControl.i18n';
+
+interface InlineCreateGoalControl extends React.HTMLAttributes<HTMLDivElement> {
+    projectId: string;
+}
+
+const StyledInlineTriggerWrapper = styled.div`
+    padding-left: ${gapSm};
+`;
+
+export const InlineCreateGoalControl: React.FC<InlineCreateGoalControl> = ({ projectId, ...attrs }) => (
+    <StyledInlineTriggerWrapper {...attrs} {...createGoalInlineControl.attr}>
+        <InlineTrigger
+            text={tr('Create goal')}
+            onClick={dispatchModalEvent(ModalEvent.GoalCreateModal, {
+                id: projectId,
+            })}
+            icon={<IconPlusCircleOutline noWrap size="s" />}
+        />
+    </StyledInlineTriggerWrapper>
+);
