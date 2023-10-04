f2f2d005f1d4b3cbd40c39f75db5e37af0152032
Maksim Sviridov <enemyworld@gmail.com>
2023-10-04 13:31:23 +0300

    test: add db tasks
    ci: add env for init prisma

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from 'cypress';
 import * as dotenv from 'dotenv';
 
+import { initDb } from './cypress/plugins';
+
 dotenv.config();
 
 export default defineConfig({
     env: {
         ADMIN_EMAIL: process.env.ADMIN_EMAIL,
         ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+        defaultPriority: 'Medium',
+        currentUser: {
+            user: {
+                email: process.env.ADMIN_EMAIL,
+            },
+        },
     },
     e2e: {
         baseUrl: 'http://localhost:3000',
         supportFile: false,
+        setupNodeEvents(on) {
+            initDb(on);
+        },
     },
     video: false,
 });
