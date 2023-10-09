10f70d538a534af6c31ae42a0277f1a76fae7455
Tony Vi <winogradovaa@gmail.com>
2023-10-09 23:35:56 +0300

    refactor(UserBadge): remove unnecessary code, #1764

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,9 @@
 export type FieldDiff = [string | undefined | null, string | undefined | null];
+
+export interface UserData {
+    email: string;
+    name?: string | null;
+    nickname?: string | null;
+    image?: string | null;
+    [key: string]: unknown;
+}
