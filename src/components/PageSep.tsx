dd0584793908006a27e58328120547b9eb69fd17
Tony Vi <winogradovaa@gmail.com>
2023-10-17 19:47:21 +0300

    fix: sync dashborad and goals pages design

--- src/components/PageSep.tsx
+++ src/components/PageSep.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import { gray4 } from '@taskany/colors';
 
 export const PageSep = styled.div`
-    border-top: 1px solid ${gray4};
     margin: 16px 0px;
+
+    box-sizing: border-box;
+
+    border-top: 1px solid ${gray4};
 `;
