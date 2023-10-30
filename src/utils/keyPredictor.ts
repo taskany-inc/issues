db34e88046c6f1142eeab08410602cfcdd85d98d
DenisVorop <saskeazaske@icloud.com>
2023-10-30 19:27:36 +0300

    feat(keyPredictor): supported transliteration

--- src/utils/keyPredictor.ts
+++ src/utils/keyPredictor.ts
@@ -1,3 +1,41 @@
+const consonantsTransliterationMap: Record<string, string> = {
+    б: 'b',
+    в: 'v',
+    г: 'g',
+    д: 'd',
+    ж: 'zh',
+    з: 'z',
+    й: 'y',
+    к: 'k',
+    л: 'l',
+    м: 'm',
+    н: 'n',
+    п: 'p',
+    р: 'r',
+    с: 's',
+    т: 't',
+    ф: 'f',
+    х: 'h',
+    ц: 'ts',
+    ч: 'ch',
+    ш: 'sh',
+    щ: 'sch',
+    ь: '',
+    ъ: '',
+};
+
+const vowelsTransliterationMap: Record<string, string> = {
+    а: 'a',
+    е: 'e',
+    и: 'i',
+    о: 'o',
+    у: 'u',
+    ы: 'y',
+    э: 'e',
+    ю: 'yu',
+    я: 'ya',
+};
+
 const allowedSymbols = {
     consonantsAndNumbers: new Set('qwrtpsdfghjklzxcvbnm1234567890'),
     vowels: new Set('aeiouy'),
@@ -5,6 +43,10 @@ const allowedSymbols = {
 
 export const keyPredictor = (str: string, { allowVowels } = { allowVowels: false }) => {
     const allowedChars = new Set(allowedSymbols.consonantsAndNumbers);
+    const allowedTransliterationChars = {
+        ...consonantsTransliterationMap,
+        ...(allowVowels ? vowelsTransliterationMap : undefined),
+    };
 
     if (allowVowels) {
         allowedSymbols.vowels.forEach((ch) => allowedChars.add(ch));
@@ -13,6 +55,10 @@ export const keyPredictor = (str: string, { allowVowels } = { allowVowels: false
     const result = [];
 
     for (const ch of str.toLowerCase()) {
+        if (allowedTransliterationChars[ch]) {
+            result.push(...allowedTransliterationChars[ch].split(''));
+        }
+
         if (allowedChars.has(ch)) {
             result.push(ch);
         }
