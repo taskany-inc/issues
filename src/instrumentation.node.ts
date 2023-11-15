ec354c7e0ae3f2908d2d0d95045502196105f810
rpg59 <vanxellshing24@gmail.com>
2023-11-15 15:45:19 +0300

    feat: improve opentelemetry dx

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -4,15 +4,18 @@ import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 
-const serviceEnvironment = process.env.SERVICE_ENVIRONMENT || 'unknown';
-const commitHash = process.env.CI_COMMIT_SHORT_SHA || 'unknown';
+if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+    const sdk = new NodeSDK({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]:
+                process.env.TASKANY_OPEN_TELEMETRY_SERVICE_NAME || 'taskany-issues',
+            [SemanticResourceAttributes.SERVICE_VERSION]: process.env.SERVICE_VERSION,
+            [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT,
+            [SemanticResourceAttributes.K8S_POD_NAME]: process.env.HOSTNAME,
+            [SemanticResourceAttributes.K8S_CLUSTER_NAME]: process.env.CLUSTER_URL,
+        }),
+        spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
+    });
 
-const sdk = new NodeSDK({
-    resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: `issues-${serviceEnvironment}-${commitHash}`,
-        [SemanticResourceAttributes.K8S_POD_NAME]: process.env.HOSTNAME,
-        [SemanticResourceAttributes.K8S_CLUSTER_NAME]: process.env.CLUSTER_URL,
-    }),
-    spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
-});
-sdk.start();
+    sdk.start();
+}
