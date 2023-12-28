662c78930dc2ebe436fc3449d6ae9674bf80bfaf
Anton <sab94a@yandex.ru>
2023-12-28 16:01:43 +0300

    feat: test commmit

--- src/utils/worker/resolve.ts
+++ src/utils/worker/resolve.ts
@@ -1,6 +1,5 @@
 import { Role, StateType } from '@prisma/client';
 
-import { addCalculatedGoalsFields } from '../../../trpc/queries/goals';
 import { prisma } from '../prisma';
 import { goalIncludeCriteriaParams, recalculateCriteriaScore } from '../recalculateCriteriaScore';
 import { prepareRecipients } from '../prepareRecipients';
@@ -9,6 +8,50 @@ import { createEmailJob, JobDataMap } from './create';
 import { goalPingJob } from './goalPingJob';
 import { sendMail } from './mail';
 import * as emailTemplates from './mail/templates';
+
+export const addCommonCalculatedGoalFields = (goal: any) => {
+    const _shortId = `${goal.projectId}-${goal.scopeId}`;
+    const _hasAchievementCriteria = !!goal.goalAchiveCriteria?.length;
+
+    return {
+        _shortId,
+        _hasAchievementCriteria,
+    };
+};
+
+export const addCalculatedGoalsFields = (goal: any, activityId: string, role: Role) => {
+    const _isOwner = goal.ownerId === activityId;
+    const _isParticipant = goal.participants?.some((participant: any) => participant?.id === activityId);
+    const _isWatching = goal.watchers?.some((watcher: any) => watcher?.id === activityId);
+    const _isStarred = goal.stargizers?.some((stargizer: any) => stargizer?.id === activityId);
+    const _isIssuer = goal.activityId === activityId;
+
+    let parentOwner = false;
+    function checkParent(project?: any) {
+        if (project?.activityId === activityId) {
+            parentOwner = true;
+        }
+
+        if (project?.parent?.length) {
+            project?.parent.forEach((p: any) => {
+                checkParent(p);
+            });
+        }
+    }
+    checkParent(goal.project);
+
+    const _isEditable = _isOwner || _isIssuer || parentOwner || role === 'ADMIN';
+
+    return {
+        _isOwner,
+        _isParticipant,
+        _isWatching,
+        _isStarred,
+        _isIssuer,
+        _isEditable,
+        ...addCommonCalculatedGoalFields(goal),
+    };
+};
 
 export const updateProjectUpdatedAt = async (id?: string | null) => {
     if (!id) return;
