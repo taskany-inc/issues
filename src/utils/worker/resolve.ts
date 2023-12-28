ff18445dbb7782bdb5206122b5a25e6f1f9edbed
Anton <sab94a@yandex.ru>
2023-12-28 15:48:02 +0300

    feat: test commmit

--- src/utils/worker/resolve.ts
+++ src/utils/worker/resolve.ts
@@ -1,9 +1,163 @@
-import { createComment } from '../db/createComment';
+import { Role, StateType } from '@prisma/client';
 
-import * as emailTemplates from './mail/templates';
-import { sendMail } from './mail';
-import { JobDataMap } from './create';
+import { addCalculatedGoalsFields } from '../../../trpc/queries/goals';
+import { prisma } from '../prisma';
+import { goalIncludeCriteriaParams, recalculateCriteriaScore } from '../recalculateCriteriaScore';
+import { prepareRecipients } from '../prepareRecipients';
+
+import { createEmailJob, JobDataMap } from './create';
 import { goalPingJob } from './goalPingJob';
+import { sendMail } from './mail';
+import * as emailTemplates from './mail/templates';
+
+export const updateProjectUpdatedAt = async (id?: string | null) => {
+    if (!id) return;
+
+    return prisma.project.update({
+        where: { id },
+        data: { id },
+    });
+};
+
+export const createComment = async ({
+    description,
+    stateId,
+    activityId,
+    goalId,
+    role,
+    shouldUpdateGoal = true,
+}: {
+    description: string;
+    stateId?: string;
+    activityId: string;
+    role: Role;
+    goalId: string;
+    shouldUpdateGoal?: boolean;
+}) => {
+    const [commentAuthor, actualGoal, pushState] = await Promise.all([
+        prisma.activity.findUnique({
+            where: { id: activityId },
+            include: { user: true, ghost: true },
+        }),
+        prisma.goal.findUnique({
+            where: { id: goalId },
+            include: {
+                participants: { include: { user: true, ghost: true } },
+                watchers: { include: { user: true, ghost: true } },
+                activity: { include: { user: true, ghost: true } },
+                owner: { include: { user: true, ghost: true } },
+                state: true,
+                project: true,
+                goalInCriteria: goalIncludeCriteriaParams,
+            },
+        }),
+        stateId ? prisma.state.findUnique({ where: { id: stateId } }) : Promise.resolve(undefined),
+    ]);
+
+    if (!commentAuthor) return null;
+    if (!actualGoal) return null;
+
+    const { _isEditable, _shortId } = addCalculatedGoalsFields(actualGoal, activityId, role);
+
+    const commentCreateOperation = prisma.comment.create({
+        data: {
+            description,
+            activityId: commentAuthor.id,
+            goalId,
+            stateId: _isEditable ? stateId : undefined,
+        },
+        include: {
+            activity: {
+                include: {
+                    user: true,
+                },
+            },
+        },
+    });
+
+    const [updatedGoal, newComment] =
+        stateId || shouldUpdateGoal
+            ? await prisma.$transaction([
+                  prisma.goal.update({
+                      where: { id: goalId },
+                      data: {
+                          id: goalId,
+                          stateId: _isEditable ? pushState?.id : actualGoal.stateId,
+                          goalInCriteria: {
+                              updateMany: {
+                                  where: {
+                                      id: { in: actualGoal.goalInCriteria.map(({ id }) => id) },
+                                  },
+                                  data: {
+                                      isDone: _isEditable && pushState?.type && pushState?.type === StateType.Completed,
+                                  },
+                              },
+                          },
+                          history:
+                              _isEditable && stateId && stateId !== actualGoal.stateId
+                                  ? {
+                                        create: {
+                                            subject: 'state',
+                                            action: 'change',
+                                            previousValue: actualGoal.stateId,
+                                            nextValue: stateId,
+                                            activityId,
+                                        },
+                                    }
+                                  : undefined,
+                          // subscribe comment author
+                          watchers: {
+                              connect: [{ id: commentAuthor.id }],
+                          },
+                      },
+                      include: {
+                          goalInCriteria: goalIncludeCriteriaParams,
+                          state: true,
+                      },
+                  }),
+                  commentCreateOperation,
+              ])
+            : await Promise.all([Promise.resolve(actualGoal), commentCreateOperation]);
+
+    await updateProjectUpdatedAt(updatedGoal.projectId);
+
+    if (_isEditable && stateId && stateId !== updatedGoal.stateId) {
+        await recalculateCriteriaScore(updatedGoal.id).recalcLinkedGoalsScores().recalcAverageProjectScore().run();
+    }
+
+    if (newComment.activity.user) {
+        const recipients = prepareRecipients(
+            [...actualGoal.participants, ...actualGoal.watchers, actualGoal.activity, actualGoal.owner],
+            newComment.activity.user.email,
+        );
+
+        if (stateId) {
+            await createEmailJob('goalStateUpdatedWithComment', {
+                to: recipients,
+                shortId: _shortId,
+                stateTitleBefore: actualGoal.state?.title,
+                stateTitleAfter: updatedGoal.state?.title,
+                title: actualGoal.title,
+                commentId: newComment.id,
+                author: newComment.activity.user.name || newComment.activity.user.email,
+                authorEmail: newComment.activity.user.email,
+                body: newComment.description,
+            });
+        } else {
+            await createEmailJob('goalCommented', {
+                to: recipients,
+                shortId: _shortId,
+                title: actualGoal.title,
+                commentId: newComment.id,
+                author: newComment.activity.user.name || newComment.activity.user.email,
+                authorEmail: newComment.activity.user.email,
+                body: newComment.description,
+            });
+        }
+    }
+
+    return newComment;
+};
 
 export const email = async ({ template, data }: JobDataMap['email']) => {
     const renderedTemplate = await emailTemplates[template](data);
