aa31c53ad99393cdeb5b04f4764602394b451f06
Tony Vi <winogradovaa@gmail.com>
2024-02-08 02:41:57 +0300

    chore(State): add theme based colors to db model

---
+++ prisma/migrations/20240207173340_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "State" ADD COLUMN     "darkBackground" TEXT,
+ADD COLUMN     "darkForeground" TEXT,
+ADD COLUMN     "lightBackground" TEXT,
+ADD COLUMN     "lightForeground" TEXT;
