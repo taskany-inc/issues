9892306ebd228ea3485091676407a52d14d820ea
Katrinputrina <putrina.katya@gmail.ru>
2024-02-22 22:10:54 +0300

    feat(OfflineBanner): update offline banner from bricks

--- src/components/OfflineBanner/OfflineBanner.tsx
+++ src/components/OfflineBanner/OfflineBanner.tsx
@@ -5,13 +5,15 @@ import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
 import { tr } from './OfflineBanner.i18n';
 
 export const OfflineBanner = () => {
-    const [globalOnlineStatus, remoteServerStatus] = useOfflineDetector({
-        setStatus: () => {
-            onlineManager.setOnline(globalOnlineStatus || remoteServerStatus);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, remoteServerStatus] = useOfflineDetector({
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        setStatus: ([_global, remote]) => {
+            onlineManager.setOnline(remote);
         },
         remoteServerUrl: '/api/health',
     });
-    return nullable(!globalOnlineStatus || !remoteServerStatus, () => (
+    return nullable(!remoteServerStatus, () => (
         <OfflineBannerBricks text={tr('You are currently offline. Check connection.')} />
     ));
 };
