85e56efa557d18be62f2e033ac2c635ba1207901
Anton <sab94a@yandex.ru>
2024-02-26 17:50:44 +0300

    feat: team units

--- src/components/TeamListItem.tsx
+++ src/components/TeamListItem.tsx
@@ -1,16 +1,17 @@
 import { FC, useCallback } from 'react';
 import { Button, Text } from '@taskany/bricks/harmony';
 import { IconBinOutline } from '@taskany/icons';
-import { TableCell } from '@taskany/bricks';
+import { TableCell, TableRow } from '@taskany/bricks';
 
 import { TableRowItem } from './Table';
 
 interface TeamListItemProps {
     name: string;
+    units: number;
     onRemoveClick: () => void;
 }
 
-export const TeamListItem: FC<TeamListItemProps> = ({ name, onRemoveClick }) => {
+export const TeamListItem: FC<TeamListItemProps> = ({ name, units, onRemoveClick }) => {
     const onClick = useCallback(
         (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
             e.preventDefault();
@@ -20,9 +21,12 @@ export const TeamListItem: FC<TeamListItemProps> = ({ name, onRemoveClick }) => 
     );
     return (
         <TableRowItem title={<Text size="l">{name}</Text>}>
-            <TableCell justify="end">
-                <Button onClick={onClick} size="xs" iconLeft={<IconBinOutline size="xs" />} />
-            </TableCell>
+            <TableRow>
+                <TableCell justify="end">{units}</TableCell>
+                <TableCell justify="end" width={65}>
+                    <Button onClick={onClick} size="xs" iconLeft={<IconBinOutline size="xs" />} />
+                </TableCell>
+            </TableRow>
         </TableRowItem>
     );
 };
