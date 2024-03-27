7122f33f9bf51d4550e5a4d4ad739067c2bb4727
Maksim Sviridov <enemyworld@gmail.com>
2024-03-27 14:22:50 +0300

    fix(NexlLink): ref link wrapper

--- src/components/NextLink.tsx
+++ src/components/NextLink.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextLinkOriginal from 'next/link';
-import { Link } from '@taskany/bricks';
+import { Link } from '@taskany/bricks/harmony';
+import { forwardRef } from 'react';
 
-export const NextLink = ({
-    inline,
-    forwardRef,
-    as,
-    ...props
-}: React.ComponentProps<typeof Link> & { forwardRef?: React.Ref<HTMLAnchorElement>; href: string }) => (
-    <NextLinkOriginal {...props} />
+export const NextLink = forwardRef<HTMLAnchorElement, React.ComponentPropsWithoutRef<typeof Link> & { href: string }>(
+    ({ href, ...props }, ref) => (
+        <NextLinkOriginal legacyBehavior passHref href={href}>
+            <Link href={href} {...props} ref={ref} />
+        </NextLinkOriginal>
+    ),
 );
