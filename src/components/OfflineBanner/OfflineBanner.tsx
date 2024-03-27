67b400ad445ae12d2f2f35099fad9e06d195b968
Maksim Sviridov <enemyworld@gmail.com>
2024-03-27 14:15:08 +0300

    fix(OfflineBanner): infinity requests

--- src/components/OfflineBanner/OfflineBanner.tsx
+++ src/components/OfflineBanner/OfflineBanner.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { onlineManager } from '@tanstack/react-query';
 import { nullable, useOfflineDetector } from '@taskany/bricks';
 import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
@@ -5,14 +6,20 @@ import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
 import { tr } from './OfflineBanner.i18n';
 
 export const OfflineBanner = () => {
+    const isOnline = useRef<boolean | null>(null);
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_, remoteServerStatus] = useOfflineDetector({
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setStatus: ([_global, remote]) => {
-            onlineManager.setOnline(remote);
+            if (isOnline.current !== remote) {
+                isOnline.current = remote;
+                onlineManager.setOnline(remote);
+            }
         },
         remoteServerUrl: '/api/health',
     });
+
     return nullable(!remoteServerStatus, () => (
         <OfflineBannerBricks text={tr('You are currently offline. Check connection.')} />
     ));
