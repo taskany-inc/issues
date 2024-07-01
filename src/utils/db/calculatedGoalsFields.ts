e9dee3c901a666b1d726bad63f78c9dc80ad3491
Anton Sabotovich <sab94a@yandex.ru>
2024-07-01 14:58:09 +0300

    fix: add access to participants

--- src/utils/db/calculatedGoalsFields.ts
+++ src/utils/db/calculatedGoalsFields.ts
@@ -17,28 +17,41 @@ export const addCommonCalculatedGoalFields = (goal: any) => {
     };
 };
 
-export const addCalculatedGoalsFields = (goal: any, activityId: string, role: Role) => {
+interface WithId {
+    id: string;
+}
+
+export const addCalculatedGoalsFields = <
+    T extends {
+        ownerId: string | null;
+        participants?: WithId[];
+        watchers?: WithId[];
+        stargizers?: WithId[];
+        activityId: string | null;
+        project?: P | null;
+    },
+    P extends {
+        participants?: WithId[];
+        activityId: string | null;
+        parent?: P[];
+    },
+>(
+    goal: T,
+    activityId: string,
+    role: Role,
+) => {
     const _isOwner = goal.ownerId === activityId;
-    const _isParticipant = goal.participants?.some((participant: any) => participant?.id === activityId);
-    const _isWatching = goal.watchers?.some((watcher: any) => watcher?.id === activityId);
-    const _isStarred = goal.stargizers?.some((stargizer: any) => stargizer?.id === activityId);
+    const _isParticipant = goal.participants?.some((participant) => participant?.id === activityId);
+    const _isWatching = goal.watchers?.some((watcher) => watcher?.id === activityId);
+    const _isStarred = goal.stargizers?.some((stargizer) => stargizer?.id === activityId);
     const _isIssuer = goal.activityId === activityId;
 
-    let parentOwner = false;
-    function checkParent(project?: any) {
-        if (project?.activityId === activityId) {
-            parentOwner = true;
-        }
+    const parentOwner = goal.project?.activityId === activityId;
+    const parentParticipant = goal.project?.participants?.some(
+        (participant: { id: string }) => participant?.id === activityId,
+    );
 
-        if (project?.parent?.length) {
-            project?.parent.forEach((p: any) => {
-                checkParent(p);
-            });
-        }
-    }
-    checkParent(goal.project);
-
-    const _isEditable = _isParticipant || _isOwner || _isIssuer || parentOwner || role === 'ADMIN';
+    const _isEditable = _isParticipant || parentParticipant || _isOwner || _isIssuer || parentOwner || role === 'ADMIN';
 
     return {
         _isOwner,
