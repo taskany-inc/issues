0b3a9a53f202f7bcc8392fd8fafef63281a91df9
Maksim Sviridov <enemyworld@gmail.com>
2024-07-26 13:34:48 +0300

    feat(SortList): brand new component

---
+++ src/components/SortList/SortList.tsx
@@ -0,0 +1,73 @@
+import React, { useCallback, useMemo } from 'react';
+import { AutoComplete, AutoCompleteList } from '@taskany/bricks/harmony';
+
+import type { FilterQueryState, SortableProps, SortDirection } from '../../hooks/useUrlFilterParams';
+import { SortButton } from '../SortButton/SortButton';
+
+import { tr } from './SortList.i18n';
+import styles from './SortList.module.css';
+
+interface SortListProps {
+    value?: FilterQueryState['sortParams'];
+    onChange: (key: SortableProps, dir: SortDirection | null) => void;
+}
+
+interface SingleSortItem {
+    id: SortableProps;
+    title: string;
+    dir: SortDirection | null;
+}
+
+export const SortList: React.FC<SortListProps> = ({ value, onChange }) => {
+    const sortItems: { [K in SortableProps]: string } = useMemo(
+        () => ({
+            title: tr('Title'),
+            state: tr('State'),
+            priority: tr('Priority'),
+            project: tr('Project'),
+            activity: tr('Activity'),
+            owner: tr('Owner'),
+            updatedAt: tr('UpdatedAt'),
+            createdAt: tr('CreatedAt'),
+        }),
+        [],
+    );
+
+    const itemsToRender: SingleSortItem[] = useMemo(() => {
+        return (Object.entries(sortItems) as Array<[SortableProps, string]>).map(([id, title]) => ({
+            id,
+            title,
+            dir: null,
+        }));
+    }, [sortItems]);
+
+    const selected: SingleSortItem[] | undefined = useMemo(() => {
+        return value?.map(({ key, dir }) => ({ id: key, title: sortItems[key], dir }));
+    }, [value, sortItems]);
+
+    const handleChange = useCallback(
+        (key: SortableProps) => (dir: SortDirection | null) => {
+            onChange(key, dir);
+        },
+        [onChange],
+    );
+
+    return (
+        <div className={styles.SortList}>
+            <AutoComplete
+                items={itemsToRender}
+                value={selected}
+                renderItem={({ item }) => (
+                    <SortButton title={item.title} value={item.dir} onChange={handleChange(item.id)} />
+                )}
+            >
+                <div className={styles.SortListButtons}>
+                    <AutoCompleteList selected />
+                </div>
+                <div className={styles.SortListButtons}>
+                    <AutoCompleteList filterSelected />
+                </div>
+            </AutoComplete>
+        </div>
+    );
+};
