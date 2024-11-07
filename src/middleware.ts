76607c4525be2ecb483c6c3b1687226a0771daba
Timur Minulin <ns16@304.ru>
2024-11-07 13:50:29 +0100

    feat(INTERNAL-1100): move event processing out of middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,59 +1,30 @@
 import { getSession } from 'next-auth/react';
-import { NextResponse, userAgent } from 'next/server';
+import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+import { processEvent } from './utils/analyticsEvent';
+
 export async function middleware(request: NextRequest) {
-    const telemetryURL = process.env.TELEMETRY_URL;
+    const requestForNextAuth = {
+        headers: {
+            cookie: request.headers.get('cookie') ?? undefined,
+        },
+    };
+    const session = await getSession({ req: requestForNextAuth });
 
-    if (telemetryURL) {
-        const requestForNextAuth = {
-            headers: {
-                cookie: request.headers.get('cookie') ?? undefined,
-            },
-        };
-        const session = await getSession({ req: requestForNextAuth });
-        const ua = userAgent(request);
+    const {
+        url,
+        nextUrl: { searchParams, pathname },
+    } = request;
 
-        const { url } = request;
-        const parts = request.url.split('/');
-        let locale = parts[3];
-        const host = parts[2];
-        let path = parts.slice(4).join('/');
-        if (!['ru', 'en'].includes(locale)) {
-            locale = 'en';
-            path = parts.slice(3).join('/');
-        }
-
-        const params = {
-            event_type: 'pageview',
-            event_properties: {
-                service: 'issues',
-                user_id: session?.user?.id,
-                session_id: session?.expires,
-                time: Date.now(),
-                user_agent: ua.ua,
-                url,
-                locale,
-                host,
-                path,
-                appHost: process.env.NEXTAUTH_URL,
-            },
-            device_type: ua.device.type,
-            device_brand: ua.device.vendor,
-            device_model: ua.device.model,
-            os_name: ua.os.name,
-            os_version: ua.os.version,
-        };
-
-        fetch(telemetryURL, {
-            body: JSON.stringify({ events: [params] }),
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        });
-    }
+    processEvent({
+        eventType: searchParams.size === 0 ? 'pageview' : 'query',
+        url,
+        pathname,
+        searchParams: Object.fromEntries(searchParams),
+        session,
+        uaHeader: request.headers.get('user-agent') || undefined,
+    });
 
     const response = NextResponse.next();
 
@@ -70,7 +41,7 @@ export const config = {
          * - favicon.ico (favicon file)
          */
         {
-            source: '/((?!api|_next/static|_next/image|favicon|theme).*)',
+            source: '/((?!api|_next/static|_next/image|_next/data|favicon|theme).*)',
             missing: [
                 { type: 'header', key: 'next-router-prefetch' },
                 { type: 'header', key: 'purpose', value: 'prefetch' },
---
+++ src/utils/analyticsEvent.ts
@@ -0,0 +1,113 @@
+import { userAgentFromString } from 'next/server';
+
+import { TrpcContext } from '../../trpc/context';
+
+type EventType =
+    | 'pageview'
+    | 'query'
+    | 'click'
+    | 'searchQuery'
+    | 'userRequestCreate'
+    | 'userActiveUpdate'
+    | 'groupCreate';
+
+export interface AnalyticsEvent {
+    event_type: EventType;
+    event_properties: {
+        service: 'issues';
+        time: number;
+        user_agent: string;
+        url: string;
+        host: string;
+        appHost: string;
+        user_id?: string | number;
+        session_id?: string | number;
+        path?: string;
+        locale?: string;
+        [key: string]: string | number | undefined;
+    };
+    device_type?: string;
+    device_brand?: string;
+    device_model?: string;
+    os_name?: string;
+    os_version?: string;
+}
+
+interface ProcessEventOptions {
+    eventType: EventType;
+    url: string;
+    session: TrpcContext['session'];
+    pathname?: string;
+    searchParams?: Record<string, string | number>;
+    uaHeader?: string;
+    additionalData?: Record<string, string>;
+}
+
+const constructEvent = ({
+    eventType,
+    url,
+    pathname,
+    searchParams,
+    session,
+    uaHeader,
+    additionalData,
+}: ProcessEventOptions) => {
+    const parts = url.split('/');
+    let locale = parts[3];
+    const host = parts[2];
+    let path = parts.slice(4).join('/');
+    if (!['ru', 'en'].includes(locale)) {
+        locale = 'en';
+        path = parts.slice(3).join('/');
+    }
+
+    const ua = userAgentFromString(uaHeader);
+
+    const params: AnalyticsEvent = {
+        event_type: eventType,
+        event_properties: {
+            service: 'issues',
+            user_id: session?.user?.id,
+            session_id: session?.expires,
+            time: Date.now(),
+            user_agent: ua.ua,
+            url,
+            path: pathname || path,
+            query: JSON.stringify(searchParams),
+            locale,
+            host,
+            appHost: process.env.NEXTAUTH_URL || '',
+            ...additionalData,
+        },
+        device_type: ua.device.type,
+        device_brand: ua.device.vendor,
+        device_model: ua.device.model,
+        os_name: ua.os.name,
+        os_version: ua.os.version,
+    };
+
+    return params;
+};
+
+export const trackEvent = (events: AnalyticsEvent[]) => {
+    const telemetryURL = process.env.TELEMETRY_URL;
+
+    if (telemetryURL) {
+        fetch(telemetryURL, {
+            body: JSON.stringify({ events }),
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+    } else {
+        console.log('TELEMETRY EVENT', JSON.stringify(events));
+    }
+};
+
+export const processEvent = (options: ProcessEventOptions) => {
+    const fullEvent = constructEvent(options);
+
+    trackEvent([fullEvent]);
+};
