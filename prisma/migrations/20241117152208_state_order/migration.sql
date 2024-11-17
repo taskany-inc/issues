421c26a06dad53f82a87540c67180dbc6002a2f9
Goriany Igor <mr.q.u.b.e31@gmail.com>
2024-11-17 18:23:05 +0300

    feat(INTERNAL-1360): add order to State

---
+++ prisma/migrations/20241117152208_state_order/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "State" ADD COLUMN     "order" SERIAL NOT NULL;
--- trpc/router/state.ts
+++ trpc/router/state.ts
@@ -2,5 +2,5 @@ import { db } from '../connection/kysely';
 import { protectedProcedure, router } from '../trpcBackend';
 
 export const state = router({
-    all: protectedProcedure.query(async () => db.selectFrom('State').selectAll().execute()),
+    all: protectedProcedure.query(async () => db.selectFrom('State').selectAll().orderBy('State.order asc').execute()),
 });
