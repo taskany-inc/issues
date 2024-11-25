df8f338648ac191363114c01a69fb8b5eb91e615
tsumo <tsumo@users.noreply.github.com>
2024-11-25 16:13:58 +0300

    feat(INTERNAL-1361): whats new in separate trpc call

--- src/utils/trpcClient.ts
+++ src/utils/trpcClient.ts
@@ -1,4 +1,4 @@
-import { httpBatchLink } from '@trpc/client';
+import { httpBatchLink, httpLink, splitLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 
 import type { TrpcRouter } from '../../trpc/router';
@@ -15,6 +15,25 @@ function getBaseUrl() {
 
 export const trpc = createTRPCNext<TrpcRouter>({
     config: ({ ctx }) => {
+        const linkOptions = {
+            url: `${getBaseUrl()}/api/trpc`,
+            headers: async () => {
+                if (ctx?.req) {
+                    // https://trpc.io/docs/nextjs/ssr#q-why-do-i-need-to-delete-the-connection-header-when-using-ssr-on-node-18
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const { connection, ...headers } = ctx.req.headers;
+
+                    return {
+                        ...headers,
+                        // Optional: inform server that it's an SSR request
+                        'x-ssr': '1',
+                    };
+                }
+
+                return {};
+            },
+        };
+
         return {
             transformer,
 
@@ -32,23 +51,12 @@ export const trpc = createTRPCNext<TrpcRouter>({
             },
 
             links: [
-                httpBatchLink({
-                    url: `${getBaseUrl()}/api/trpc`,
-                    headers: async () => {
-                        if (ctx?.req) {
-                            // https://trpc.io/docs/nextjs/ssr#q-why-do-i-need-to-delete-the-connection-header-when-using-ssr-on-node-18
-                            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                            const { connection, ...headers } = ctx.req.headers;
-
-                            return {
-                                ...headers,
-                                // Optional: inform server that it's an SSR request
-                                'x-ssr': '1',
-                            };
-                        }
-
-                        return {};
+                splitLink({
+                    condition: (op) => {
+                        return op.context.skipBatch === true;
                     },
+                    true: httpLink(linkOptions),
+                    false: httpBatchLink(linkOptions),
                 }),
             ],
         };
